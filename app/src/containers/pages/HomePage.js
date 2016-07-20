d6418f7bd50b474beecb77b858b0f414f08f919a
j8seangel <jseangel@gmail.com>
2016-07-20 12:44:50 +0200

    Remove unnecesary containers

--- app/src/containers/pages/HomePage.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import HomePage from '../../components/pages/HomePage';
-
-const mapStateToProps = (state, location) => ({
-  currentPage: location.route.path
-});
-const mapDispatchToProps = () => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
