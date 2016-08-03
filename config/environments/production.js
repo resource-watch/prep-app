4c7a7268e29d5a21c59435b969870472d2c9773f
David Inga <davidsingal@icloud.com>
2016-08-03 13:43:34 +0200

    added favicon

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 
 const indexPath = path.join(process.cwd(), 'dist/index.html');
 
 module.exports = (app) => {
+  app.use(favicon(process.cwd() + '/public/favicon.ico'));
   app.use(express.static(path.join(process.cwd(), 'dist')));
   app.get('*', (req, res) => {
     res.sendFile(indexPath);
