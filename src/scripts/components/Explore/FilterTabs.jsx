e2dff16d32d89a83a651618a0c19cff2360e798a
j8seangel <jseangel@gmail.com>
2016-08-23 12:27:15 +0200

    Add filters styles

--- src/scripts/components/Explore/FilterTabs.jsx
+++ src/scripts/components/Explore/FilterTabs.jsx
@@ -5,15 +5,21 @@ class FilterTabs extends React.Component {
   constructor() {
     super();
 
+    this.state = {
+      filterOpen: false,
+      filterSelected: ''
+    };
+
+    this.filtersName = {
+      topics: 'topics',
+      spatialExtent: 'spatial extend',
+      dataType: 'data type'
+    };
+
     this.filters = {
       topics: { 'flooding': 'Flooding', 'Energy': 'Energy', 'weather-climate': 'Weather climate', 'infrastructure': 'Infrastructure' },
       spatialExtent: { global: 'Global', country: 'Country', region: 'Region' },
       dataType: { model: 'Model', observation: 'Observation', historical: 'Historical', projections: 'Projections' }
-    };
-
-    this.state = {
-      filterOpen: false,
-      filterSelected: ''
     };
   }
 
@@ -48,7 +54,9 @@ class FilterTabs extends React.Component {
     Object.keys(filters).forEach((key, index) => {
       layers.push((
         <li key={`filter-tag-${index}`} onClick={() => this.onClickTag(key)}>
-          <span className={filterChoosen && filterChoosen.indexOf(key) > -1 ? 'selected' : ''}></span>
+          <span className={filterChoosen && filterChoosen.indexOf(key) > -1 ? 'checkbox -selected' : 'checkbox'}>
+            <svg width="13" height="9" viewBox="0 0 13 9" xmlns="http://www.w3.org/2000/svg"><title>Selected</title><path d="M5.744 6.997l6.514-5.465L10.972 0 4.46 5.464 1.176 3.078 0 4.696l4.854 3.527.89-1.226z" fill="#FFF" fillRule="evenodd"/></svg>
+          </span>
           <span>{filters[key]}</span>
         </li>
       ));
@@ -67,31 +75,53 @@ class FilterTabs extends React.Component {
   render() {
     const filters = this.getFilters();
     const filterChoosen = this.props.filtersChoosen;
+
+    let pointerPosition = 30;
+    switch (this.state.filterSelected) {
+      case 'spatialExtent':
+        pointerPosition = 160;
+        break;
+      case 'dataType':
+        pointerPosition = 290;
+        break;
+      default:
+
+    }
+
     const filtersCount = {
-      topics: filterChoosen['topics'] && filterChoosen['topics'].length ? filterChoosen['topics'].length : '',
-      spatialExtent: filterChoosen['spatialExtent'] && filterChoosen['spatialExtent'].length ? filterChoosen['spatialExtent'].length : '',
-      dataType: filterChoosen['dataType'] && filterChoosen['dataType'].length ? filterChoosen['dataType'].length : '',
+      topics: filterChoosen['topics'] && filterChoosen['topics'].length ? filterChoosen['topics'].length : 0,
+      spatialExtent: filterChoosen['spatialExtent'] && filterChoosen['spatialExtent'].length ? filterChoosen['spatialExtent'].length : 0,
+      dataType: filterChoosen['dataType'] && filterChoosen['dataType'].length ? filterChoosen['dataType'].length : 0,
     };
 
     return (
       <div className="filters-tab">
-        <ul className="filters-toolbar">
+        <ul className="filters-toolbar  columns small-12">
           <li>
-            <Button themeColor click={() => this.openFilter('topics')}> Topics </Button>
-            <span>{filtersCount.topics}</span>
+            <Button themeColor click={() => this.openFilter('topics')}> {this.filtersName.topics} </Button>
+            <span className={filtersCount.topics === 0 ? '-hide' : ''}>{filtersCount.topics}</span>
           </li>
           <li>
-            <Button themeColor click={() => this.openFilter('spatialExtent')}> Spatial extent </Button>
-            <span>{filtersCount.spatialExtent}</span>
+            <Button themeColor click={() => this.openFilter('spatialExtent')}> {this.filtersName.spatialExtent} </Button>
+            <span className={filtersCount.spatialExtent === 0 ? '-hide' : ''}>{filtersCount.spatialExtent}</span>
           </li>
           <li>
-            <Button themeColor click={() => this.openFilter('dataType')}> Data type </Button>
-            <span>{filtersCount.dataType}</span>
+            <Button themeColor click={() => this.openFilter('dataType')}> {this.filtersName.dataType} </Button>
+            <span className={filtersCount.dataType === 0 ? '-hide' : ''}>{filtersCount.dataType}</span>
           </li>
         </ul>
 
         {this.state.filterOpen &&
-          <ul className="filters-list">{filters}</ul>
+          <div className="filters-list-container columns small-12">
+            <span style={{ transform: `translateX(${pointerPosition}px)` }} className="pointer"></span>
+            <h3>Filters by {this.filtersName[this.state.filterSelected]}</h3>
+            <svg className="close-button" title="Close this modal" onClick={() => this.closeFilter()}>
+              <path d="M8.047.548l-3 3L2.025.524l-1.5 1.5 3.023 3.022-3 3 1.455 1.456 3-3 3.023 3.022 1.5-1.5-3.023-3.022 3-3" fillRule="evenodd"/>
+            </svg>
+            <ul className="filters-list">
+              {filters}
+            </ul>
+          </div>
         }
       </div>
     );
