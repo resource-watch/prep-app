2780d328ded7993618d91f3ff71b675a58161832
j8seangel <jseangel@gmail.com>
2016-08-25 19:22:32 +0200

    Add drag legend and basic styles

--- app/scripts/components/Explore/ExploreLegend.jsx
+++ app/scripts/components/Explore/ExploreLegend.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { SortableContainer, SortableElement, SortableHandle, arrayMove } from 'react-sortable-hoc';
+
+const DragHandle = SortableHandle(() => <span className="handler">🔝</span>);
+
+const SortableItem = SortableElement(({layer, index, onInfoClick}) => {
+  return (
+    <div className="legend-layer row" key={`map-layer-${index}`}>
+      <div className="column small-1">
+        <DragHandle />
+      </div>
+      <div className="column small-2 align-self-middle">
+        <span className="legend"></span>
+      </div>
+      <div className="column small-7">
+        <span className="title">{layer.title}</span>
+      </div>
+      <div className="column small-2 layer-actions">
+        <Link className="icon" to={`/data/dataset/${layer.attributes['dataset-id']}`}>
+          <svg width="13" height="9" viewBox="0 0 13 9"><title>icon-eye</title><path d="M4.933 4.5c0 .855.698 1.545 1.567 1.545s1.567-.69 1.567-1.545S7.369 2.955 6.5 2.955s-1.567.69-1.567 1.545zM13 4.5C11.755 2.265 9.312 0 6.5 0 3.695 0 1.245 2.265 0 4.5 1.245 6.735 3.695 9 6.5 9c2.812 0 5.255-2.265 6.5-4.5zm-9.415 0c0-1.582 1.307-2.865 2.915-2.865S9.415 2.918 9.415 4.5c0 1.582-1.307 2.865-2.915 2.865S3.585 6.082 3.585 4.5z" fillRule="evenodd"/></svg>
+        </Link>
+        <span
+          className="icon -info"
+          onClick={() => onInfoClick(layer.attributes['dataset-id'])}
+        > i </span>
+      </div>
+    </div>
+  );
+});
+
+const SortableList = SortableContainer(({items, onInfoClick}) => {
+  return (
+    <div className="content">
+      {items.map((layer, index) =>
+        <SortableItem key={`item-${index}`} index={index} layer={layer} onInfoClick={onInfoClick} />
+      )}
+    </div>
+  );
+});
 
 class DataMapLegend extends React.Component {
   constructor() {
@@ -8,26 +46,9 @@ class DataMapLegend extends React.Component {
       legendOpen: true
     };
   }
-  getContent() {
-    const layers = [];
-    this.props.data.forEach((layer, index) => {
-      layers.push(
-        <div className="layer" key={`map-layer-${index}`}>
-          <span className="legend"></span>
-          <span className="title">{layer.title}</span>
-          <div className="actions">
-            <Link className="icon" to={`/dataset/${layer.attributes['dataset-id']}`}>
-              <svg width="13" height="9" viewBox="0 0 13 9"><title>icon-eye</title><path d="M4.933 4.5c0 .855.698 1.545 1.567 1.545s1.567-.69 1.567-1.545S7.369 2.955 6.5 2.955s-1.567.69-1.567 1.545zM13 4.5C11.755 2.265 9.312 0 6.5 0 3.695 0 1.245 2.265 0 4.5 1.245 6.735 3.695 9 6.5 9c2.812 0 5.255-2.265 6.5-4.5zm-9.415 0c0-1.582 1.307-2.865 2.915-2.865S9.415 2.918 9.415 4.5c0 1.582-1.307 2.865-2.915 2.865S3.585 6.082 3.585 4.5z" fillRule="evenodd"/></svg>
-            </Link>
-            <span
-              className="icon -info"
-              onClick={() => this.props.onInfoClick(layer.attributes['dataset-id'])}
-            > i </span>
-          </div>
-        </div>
-      );
-    });
-    return layers;
+
+  onSortEnd({ oldIndex, newIndex }) {
+    this.layers = arrayMove(this.layers, oldIndex, newIndex);
   }
 
   toggleToolbarStatus() {
@@ -38,31 +59,37 @@ class DataMapLegend extends React.Component {
 
   render() {
     let content;
+    this.layers = this.props.data.slice(0, this.props.data.length);
+
     const legendClassNames = ['c-explore-legend'];
-    if (this.state.legendOpen) legendClassNames.push('-open');
-    if (this.props.data.length) {
-      content = this.getContent();
+    if (!this.layers.length) { legendClassNames.push('-empty'); }
+
+    if (this.layers.length && this.state.legendOpen) {
+      legendClassNames.push('-open');
+      content = (<SortableList
+        axis="y"
+        lockAxis="y"
+        lockToContainerEdges
+        lockOffset="50%"
+        useDragHandle
+        items={this.layers}
+        onInfoClick={this.props.onInfoClick}
+        onSortEnd={(oldI, newI) => this.onSortEnd(oldI, newI)}
+      />);
     } else {
-      legendClassNames.push('-empty');
+      content = <div className="content"></div>;
     }
 
     return (
       <div className={legendClassNames.join(' ')}>
-        <div className="actions">
-          <div>
-            <button
-              className={['toggle-status', this.state.legendOpen ? '-open' : ''].join(' ')}
-              onClick={() => this.toggleToolbarStatus()}
-            >
-              <span></span>
-            </button>
-          </div>
+        <div className={['action-container', this.state.legendOpen ? '-open' : ''].join(' ')}>
+          <span className="help">View legend</span>
+          <span className="action open" onClick={() => this.toggleToolbarStatus()}>^</span>
+          <span className="action close" onClick={() => this.toggleToolbarStatus()}>X</span>
         </div>
-        <div className="row">
-          <div className="columns small-12 content">
-            {content}
-          </div>
-        </div>
+
+        {content}
+
       </div>
     );
   }
