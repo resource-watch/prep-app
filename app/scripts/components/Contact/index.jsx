95e271f0c47c1bd2976d9785e5d94956d1e8a3ce
j8seangel <jseangel@gmail.com>
2016-08-30 17:59:23 +0200

    Fix syntax errors

--- app/scripts/components/Contact/index.jsx
+++ app/scripts/components/Contact/index.jsx
@@ -25,10 +25,10 @@ class Contact extends React.Component {
               <div className="c-form-container">
                 <p className="error-message">Please, fill out the hightlighted fields bellow</p>
                 {!this.state.success ?
-                  <Form type='Contact' submit={()=>this.handleSubmit()}/> :
+                  <Form type="Contact" submit={() => this.handleSubmit()} /> :
                   <div className="success-message">
                     <div className="check">
-                      <svg width="13" height="9" viewBox="0 0 13 9" xmlns="http://www.w3.org/2000/svg">
+                      <svg width="13" height="9" viewBox="0 0 13 9">
                         <title>check</title>
                         <path d="M5.744 6.997l6.514-5.465L10.972 0 4.46 5.464 1.176 3.078 0 4.696l4.854 3.527.89-1.226z" />
                       </svg>
