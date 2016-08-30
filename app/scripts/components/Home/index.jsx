4848968e29421fe6a8c901caba81c09a3484286f
bitTal <albafjez@gmail.com>
2016-08-30 15:40:38 +0200

    PR review

--- app/scripts/components/Home/index.jsx
+++ app/scripts/components/Home/index.jsx
@@ -64,7 +64,7 @@ class Home extends React.Component {
         </Article>
 
         <Article grid={'small-12 medium-12'}>
-          <ExploreHome setModalUnderDevelop={()=>this.props.setModalUnderDevelop()} />
+          <ExploreHome />
         </Article>
 
         <Article grid={'small-12 medium-12'}>
