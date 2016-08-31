07d8af3753980a268cd73060c29d14732f4d35d1
bitTal <albafjez@gmail.com>
2016-08-31 10:26:00 +0200

    PR review

--- app/scripts/containers/Dataset/index.js
+++ app/scripts/containers/Dataset/index.js
@@ -3,13 +3,12 @@ import DatasetDetailPage from '../../components/Dataset';
 
 import { getDatasetDefaultWidget, getDatasetById } from '../../actions/datasets';
 
-const mapStateToProps = (state, { params, route }) => {
-
- return { currentPage: route.path,
+const mapStateToProps = (state, { params, route }) => ({
+  currentPage: route.path,
   datasetSlug: params.slug,
   data: state.datasets.details[params.slug] || null,
-  widget: state.datasets.widgets[params.slug] || null}
-};
+  widget: state.datasets.widgets[params.slug] || null
+});
 
 const mapDispatchToProps = (dispatch) => ({
   getDatasetData: (slug) => {
