7fef6ce72c30a59f76592676ab167a58b94402c5
David Inga <david.inga@vizzuality.com>
2016-12-14 17:02:06 +0100

    fixing widget api path

--- app/scripts/actions/widgets.js
+++ app/scripts/actions/widgets.js
@@ -7,7 +7,7 @@ import {
 
 export function getWidgetBySlug(slug) {
   return dispatch => {
-    fetch(`${config.apiUrl}/api/widget/${slug}`)
+    fetch(`${config.apiUrl}/api/widgets/${slug}`)
       .then(response => {
         if (response.ok) return response.json();
         throw new Error(response.statusText);
