37a2482f707ee9ceb2591ae14a74f419a64077cd
David Inga <david.inga@vizzuality.com>
2017-03-09 22:59:41 +0100

    added production env

--- config/environments/production.js
+++ config/environments/production.js
@@ -7,22 +7,22 @@ const moment = require('moment');
 const timezoneOffset = -240; // Washington
 const indexPath = path.join(process.cwd(), 'dist/index.html');
 
-function auth(username, password) {
-  return function authMiddleware(req, res, next) {
-    const now = moment().utcOffset(timezoneOffset);
-    const limit = moment({ day: 22, month: 8, year: 2016, hour: 9 })
-      .utcOffset(timezoneOffset);
-    if (now.isAfter(limit)) {
-      return next();
-    }
-    const user = basicAuth(req);
-    if (!user || user.name !== username || user.pass !== password) {
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-      return res.sendStatus(401);
-    }
-    return next();
-  };
-}
+// function auth(username, password) {
+//   return function authMiddleware(req, res, next) {
+//     const now = moment().utcOffset(timezoneOffset);
+//     const limit = moment({ day: 22, month: 8, year: 2016, hour: 9 })
+//       .utcOffset(timezoneOffset);
+//     if (now.isAfter(limit)) {
+//       return next();
+//     }
+//     const user = basicAuth(req);
+//     if (!user || user.name !== username || user.pass !== password) {
+//       res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+//       return res.sendStatus(401);
+//     }
+//     return next();
+//   };
+// }
 
 module.exports = (app) => {
   // app.use(auth(process.env.USERNAME, process.env.PASSWORD));
