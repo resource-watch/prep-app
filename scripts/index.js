920abd91c20f0a3b757d51b952d9e434800c46f4
bitTal <albafjez@gmail.com>
2017-10-19 19:00:04 +0200

    Add Partners logos

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,23 @@
 
 function getPartners() {
-  var url = '/partners';
+  var url = 'https://staging.prepdata.org/api/partners';
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    var el = document.getElementById('partners');
+    var partners = [];
+
+    if (this.readyState == 4 && this.status == 200) {
+      var partners = JSON.parse(this.responseText);
+    }
+
+    partners.forEach(function(p) {
+      el.innerHTML += '<div class="logo-container"><a href="' + p.url + '" target="_blank" rel="noreferrer noopener"><img src="' + p.images.logo + '" alt="' + p.name + '" /></a></div>';
+    }.bind(this));
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
 }
+
 
 getPartners();
