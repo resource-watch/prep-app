f1954d22ba83999cdbc0099bee8dd4f64d86be09
David Inga <david.inga@vizzuality.com>
2017-12-05 07:29:48 +0100

    basemap fix and npm packages

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -36,7 +36,8 @@ const config = {
       path.join(rootPath, 'node_modules')
     ],
     alias: {
-      styles: path.resolve('app/styles')
+      styles: path.resolve('app/styles'),
+      components: path.resolve('app/scripts/components')
     }
   },
 
@@ -63,7 +64,7 @@ const config = {
         apiUrlRW: JSON.stringify(process.env.RW_API_URL),
         basemapTileUrl: JSON.stringify(process.env.BASEMAP_TILE_URL),
         datasetEnv: JSON.stringify(process.env.DATASET_ENV),
-        assetsUrl: JSON.stringify(process.env.ASSETS_URL)
+        assetsUrl: JSON.stringify(process.env.ASSETS_URL || '')
       }
     }),
     new CopyWebpackPlugin([
