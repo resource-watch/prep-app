f0730c6c1f818c86fd581b93a577563dc64c96ad
David Inga <david.inga@vizzuality.com>
2018-01-11 12:19:05 +0100

    added combos for nexgddp layers

--- app/scripts/components/legend/legend-nexgddp-toolbar/legend-nexgddp-toolbar-component.jsx
+++ app/scripts/components/legend/legend-nexgddp-toolbar/legend-nexgddp-toolbar-component.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import find from 'lodash/find';
 import { SimpleSelect } from 'react-selectize';
 import 'react-selectize/themes/index.css';
 import './legend-nexgddp-toolbar-style.scss';
@@ -7,9 +8,20 @@ import './legend-nexgddp-toolbar-style.scss';
 class LegendNexGDDPToolbar extends PureComponent {
   constructor(props) {
     super(props);
+
     this.state = {
-      scenarios: null
+      temporalResolution: null,
+      temporalResolutionOptions: null,
+      period: null,
+      periodsOptions: null,
+      scenario: null,
+      scenariosOptions: null
     };
+
+    this.updatingPeriods = this.updatingPeriods.bind(this);
+    this.onResolutionChange = this.onResolutionChange.bind(this);
+    this.onPeriodChange = this.onPeriodChange.bind(this);
+    this.onScenarioChange = this.onScenarioChange.bind(this);
   }
 
   componentDidMount() {
@@ -23,39 +35,76 @@ class LegendNexGDDPToolbar extends PureComponent {
         if (response.ok) return response.json();
         throw Error(response);
       })
-      .then((json) => {
-        const dateData = json.temporalResolution.map(t => ({ label: t.label, value: t.id }));
-        const scenariosData = json.scenarios.map(s => ({ label: s.label, value: s.id }));
-        const scenarios = {
-          options: scenariosData,
-          selection: scenariosData[0]
-        };
-        const date = {
-          options: dateData,
-          selection: dateData[0]
-        };
-        this.setState({ scenarios, date });
-      })
+      .then(json => this.updatingCombos(json))
       .catch(error => console.error(error));
   }
 
+  onResolutionChange(temporalResolution) {
+    this.setState({ temporalResolution }, this.updatingPeriods);
+  }
+
+  onPeriodChange(period) {
+    this.setState({ period });
+  }
+
+  onScenarioChange(scenario) {
+    this.setState({ scenario });
+  }
+
+  updatingPeriods() {
+    const periodsOptions = find(this.state.temporalResolutionOptions, {
+      value: this.state.temporalResolution.value
+    }).periods.map(p => ({ label: p.label, value: p.id }));
+    this.setState({ period: periodsOptions[0], periodsOptions });
+  }
+
+  updatingCombos(data) {
+    // Temporal resolution (decadal, 30 years)
+    const temporalResolutionOptions = data.temporalResolution.map(t => ({ label: t.label, value: t.id, periods: t.periods }));
+    // Scenarios
+    const scenariosOptions = data.scenarios.map(s => ({ label: s.label, value: s.id }));
+
+    this.setState({
+      temporalResolution: temporalResolutionOptions[0],
+      temporalResolutionOptions,
+      scenario: scenariosOptions[0],
+      scenariosOptions
+    }, this.updatingPeriods);
+  }
+
   render() {
-    const { scenarios, date } = this.state;
+    const {
+      temporalResolution,
+      temporalResolutionOptions,
+      period,
+      periodsOptions,
+      scenario,
+      scenariosOptions
+    } = this.state;
+
     return (
       <div className="c-legend-nexgddp-toolbar">
-        {this.state.date && <SimpleSelect
-          name="date"
-          value={date.selection}
-          options={date.options}
-          onChange={() => {}}
+        {temporalResolutionOptions && <SimpleSelect
+          name="temporal_resolution"
+          value={temporalResolution}
+          options={temporalResolutionOptions}
+          onValueChange={this.onResolutionChange}
           theme="material"
           tether
         />}
-        {this.state.scenarios && <SimpleSelect
+        {periodsOptions && <SimpleSelect
+          name="periods"
+          value={period}
+          options={periodsOptions}
+          onValueChange={this.onPeriodChange}
+          theme="material"
+          tether
+        />}
+        {scenariosOptions && <SimpleSelect
           name="scenario"
-          value={scenarios.selection}
-          options={scenarios.options}
-          onChange={() => {}}
+          value={scenario}
+          options={scenariosOptions}
+          onValueChange={this.onScenarioChange}
           theme="material"
           tether
         />}
