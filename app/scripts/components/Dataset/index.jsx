33acb4a885c3dc421877d6176993d33f8e2233a8
David Inga <david.inga@icloud.com>
2018-01-12 18:41:13 +0100

    moving widger editor under description

--- app/scripts/components/Dataset/index.jsx
+++ app/scripts/components/Dataset/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import URI from 'urijs';
 import { connect } from 'react-redux';
 import WidgetEditor, { modalActions, SaveWidgetModal } from 'widget-editor';
+import ReactMarkdown from 'react-markdown';
 
 import TooltipTether from 'components/Tooltip/TooltipTether';
 import metadata from '../../metadata.json';
@@ -22,49 +23,6 @@ import LoadingSpinner from '../Loading/LoadingSpinner';
 import NexGDDPTool from '../nexgddp-tool/NexGDDPTool';
 
 const logoImage = '/images/prep-logo.png';
-const nexGDDPDatasets = [
-  'a0a6d98f-3cce-4a9c-b07e-ba735d1d985b',
-  'aaadd6c3-93ea-44bc-ba8b-7af3f40d39e1',
-  'b61fca59-707c-47f8-875c-9ac19313d193',
-  '0a9501d7-e2f1-46ef-922d-c8f161cc8153',
-  '4dbebe70-67a0-4bb5-ac63-88f325d8ca40',
-  'bbf2957f-55d2-4ed1-9f9d-d62de88005bb',
-  'df0aabe8-0ef2-4a81-bdba-c6fc6767fde9',
-  'eadf93a6-58e7-4482-89d6-c9832d270a87',
-  'f5152f7b-757b-4e09-98d2-e244f547fec8',
-  '2b89fe50-6795-426d-b357-df952b04294b',
-  '999fc1b8-e7aa-4e53-b291-8da47417acd1',
-  'efee89ae-584a-409f-b35e-7841d1ce9c5d',
-  '3cfc8e33-8778-40f8-a123-36d299dcaaab',
-  '648650c1-01d8-44c5-bd36-ed0ce93c0351',
-  '0b79aad3-7df6-4e38-be87-998b3d4cf62e',
-  '5c41493a-9ead-463e-b2b8-9a8bc419229b',
-  '1f596326-e5e7-484a-9d1f-572d21b9924d',
-  'c231e5fb-7bbc-4279-a21a-b7ff5679fafe',
-  '6d3dd62f-d3b8-440a-8cd2-c1087033b416',
-  '61f5e1e6-22d7-4fac-825b-d1df4d1ea841',
-  'f90f8d3d-72dc-4c89-b0b8-77cc89056a44',
-  'ffdf001f-b86c-4d60-8f2a-66d504d7bf39',
-  'a507f91c-801b-498b-a087-ab5c3e7498c7',
-  '546bf818-0857-474f-892d-25bf7253f2e3',
-  'ae2b3597-1902-41fc-b0cb-fa1a0629303a',
-  '222e109f-5e1a-43b2-89ef-27ebc79931a1',
-  'c4e7780c-2c47-4828-9c72-82240c9e37e9',
-  'ce774800-be58-4ce9-a81a-e296dfe25ab3',
-  '659377ad-4014-4415-8e43-63357541c8e8',
-  '4ede1cfb-4c26-4f0d-8fa0-055a6f07426c',
-  'a8aaae69-47ec-4117-bdfb-841c64e62d36',
-  'e156293c-95a9-4f72-96b1-8313cd5d6073',
-  'e30b788f-08f0-48cd-a254-b26c0c022447',
-  '57670934-4851-47fe-ad85-17e2b97f027f',
-  '24a64759-c823-41cd-902d-fcce00f7864d',
-  'f44d707e-7a1b-4dda-ade0-b51c0398f0ea',
-  '17d07d93-7b2a-4a1e-b285-3af903f6ee2d',
-  '8fee450f-a625-4e15-8541-f84de6205b4e',
-  'a92b3919-19f2-4431-8c0c-8ddc91ad0168',
-  '6b158f1e-0794-465a-95a3-94019b6154ce',
-  'a37b7ce6-8d14-4e3d-a57b-963557ee9ef5'
-];
 
 class DatasetDetail extends React.Component {
   static getData(key, value) {
@@ -170,11 +128,39 @@ class DatasetDetail extends React.Component {
     }
 
     const currentSection = this.props.location.state && this.props.location.state.prevPath || 'explore';
+
+    const dataset = this.props.data;
+
     return (
       <div>
         <SectionIntro data={data} downloadUrl={DatasetDetail.getDownloadUrl(this.props.data)} currentSection={currentSection} >
-          <MetadataInfo data={this.props.data} />
+          <div className="c-article">
+            <ReactMarkdown source={data.description} className="c-markdown" />
+          </div>
         </SectionIntro>
+
+        {(dataset.id && dataset.provider === 'nexgddp') ?
+          <div className="row">
+            <div className="columns small-12">
+              <NexGDDPTool dataset={dataset} />
+            </div>
+          </div> :
+          <WidgetEditor
+            datasetId={dataset.id}
+            embedButtonMode="never"
+            mapConfig={{ zoom: 3, lat: 40.65, lng: -98.21 }}
+            provideWidgetConfig={(func) => { this.getWidgetConfig = func; }}
+            onSave={() => this.onSaveWidget()}
+          />
+        }
+        <div className="row align-center">
+          <div className="columns small-12 medium-8">
+            <div className="c-article">
+              <h3>More info</h3>
+              <MetadataInfo data={dataset} />
+            </div>
+          </div>
+        </div>
 
         {/*
           (widgetComponents && widgetComponents.length) ?
@@ -230,24 +216,7 @@ class DatasetDetail extends React.Component {
         </header>
 
         <div className="l-main">
-
           {content}
-
-          {(data.id && nexGDDPDatasets.includes(data.id)) ?
-            <div className="row">
-              <div className="columns small-12">
-                <NexGDDPTool dataset={data} />
-              </div>
-            </div> :
-            <WidgetEditor
-              datasetId={data.id}
-              embedButtonMode="never"
-              mapConfig={{ zoom: 3, lat: 40.65, lng: -98.21 }}
-              provideWidgetConfig={(func) => { this.getWidgetConfig = func; }}
-              onSave={() => this.onSaveWidget()}
-            />
-          }
-
         </div>
 
         <footer className="l-footer">
