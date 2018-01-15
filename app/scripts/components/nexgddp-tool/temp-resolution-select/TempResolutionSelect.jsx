ed2555bc3aa9607d47ef346d272da4f6d6736f84
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-01-15 12:24:23 +0000

    Apply the same styles to the new selector

--- app/scripts/components/nexgddp-tool/temp-resolution-select/TempResolutionSelect.jsx
+++ app/scripts/components/nexgddp-tool/temp-resolution-select/TempResolutionSelect.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
+import './style.scss';
 
 class TempResolutionSelect extends React.PureComponent {
   render() {
