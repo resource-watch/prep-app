resource-watch/prep-app

commit dad596aac0ad8f48a3b6ca49233df6f6ce674e00

Add the events for Stories and Resources

app/scripts/components/share-control/share-control-component.jsx
@@ -12,6 +12,10 @@ class ShareControl extends Component {
     const { open } = this.props;
     event.preventDefault();
     this.props.setOpen(!open);
+    this.props.setAnalytics({
+      category: 'Explore data',
+      action: 'Share a map'
+    });
     this.props.setLinks(this.props.links);
 
     logEvent('Explore data', 'Share a map', 'Opens infowindow');
@@ -41,7 +45,8 @@ ShareControl.propTypes = {
   open: PropTypes.bool,
   links: PropTypes.object,
   setOpen: PropTypes.func,
-  setLinks: PropTypes.func
+  setLinks: PropTypes.func,
+  setAnalytics: PropTypes.func
 };
 
 export default ShareControl;

app/scripts/components/Insights/Customs/Ethiopia/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { logEvent } from 'helpers/analytics';
+
 import ChartCard from '../../../Cards/ChartCard';
 import IFrame from '../../../IFrame';
 import EmbedMap from '../../../EmbedMap';
@@ -31,6 +34,8 @@ class EthiopiaInsight extends React.Component {
   }
 
   setShareModal(url, section, widgetSlug) {
+    logEvent('Stories', `Share a ${widgetSlug} widget`, 'Opens infowindow');
+
     this.setState({
       modalShare: true,
       shareUrl: url,
@@ -262,6 +267,10 @@ class EthiopiaInsight extends React.Component {
           opened={this.state.modalShare}
           close={() => this.setState({ modalShare: false })}
           widgetSlug={this.state.widgetSlug}
+          analytics={{
+            category: 'Stories',
+            action: `Share a ${this.state.widgetSlug} widget`
+          }}
         />
       </div>
     );

app/scripts/components/Insights/InsightDetail.jsx
@@ -100,6 +100,8 @@ class InsightsDetail extends React.Component {
     if (!this.props.data) {
       return <LoadingSpinner />;
     }
+
+    const { title } = this.props.data;
     const contentUrl = this.props.data.content_url;
     const iframeUrl = contentUrl.indexOf('github.io') > -1
       ? `/proxy?url=${contentUrl}`
@@ -146,6 +148,10 @@ class InsightsDetail extends React.Component {
           insightSlug={this.props.insightSlug}
           insightUrl={this.props.data.content_url}
           currentSection={'stories'}
+          analytics={{
+            category: 'Stories',
+            action: `Shares ${title} story`
+          }}
         >
           <p> {this.props.data.summary} </p>
         </SectionIntro>

app/scripts/components/Modal/ShareModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Modal from './Modal';
 import ShareUrl from '../../containers/Modal/ShareUrl';
 
@@ -23,7 +24,7 @@ class ShareModal extends React.Component {
             'You may include this content on your webpage. To do this, copy the following html code and insert it into the source code of your page:' :
             'Click and paste link in email or IM'}
         </p>
-        <ShareUrl url={url} iframe={tab === 'embed'} />
+        <ShareUrl url={url} iframe={tab === 'embed'} analytics={this.props.analytics} />
         {tab === 'embed' ?
           openEmbed :
           (<div className="media">
@@ -82,19 +83,28 @@ ShareModal.propTypes = {
   /**
    * Define the share modal title
    */
-  title: React.PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   /**
    * Define the share modal status
    */
-  opened: React.PropTypes.bool.isRequired,
+  opened: PropTypes.bool.isRequired,
   /**
    * Define the url to share
    */
-  url: React.PropTypes.string,
+  url: PropTypes.string,
   /**
    * Define the share modal function to close the modal
    */
-  close: React.PropTypes.func.isRequired
+  close: PropTypes.func.isRequired,
+  widgetSlug: PropTypes.string,
+  /**
+   * Define the category and action for the analytics
+   * event
+   */
+  analytics: PropTypes.shape({
+    category: PropTypes.string,
+    action: PropTypes.string
+  })
 };
 
 export default ShareModal;

app/scripts/components/Modal/ShareUrl.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { logEvent } from 'helpers/analytics';
 import Button from '../Button/Button';
 import LoadingSpinner from '../Loading/LoadingSpinner';
@@ -31,7 +32,9 @@ class ShareUrl extends React.Component {
       document.execCommand('copy');
       this.setState({ copied: true });
 
-      logEvent('Explore data', 'Share a map', `Clicks to copy ${this.props.iframe ? 'embed' : 'link'}`);
+      if (this.props.analytics) {
+        logEvent(this.props.analytics.category, this.props.analytics.action, `Clicks to copy ${this.props.iframe ? 'embed' : 'link'}`);
+      }
 
       setTimeout(() => {
         this.setState({ copied: false });
@@ -76,19 +79,27 @@ ShareUrl.propTypes = {
   /**
    * Function to get the short link url
    */
-  getShortLink: React.PropTypes.func.isRequired,
+  getShortLink: PropTypes.func.isRequired,
   /**
    * Urls to generate
    */
-  url: React.PropTypes.string,
+  url: PropTypes.string,
   /**
    * Set if the url will be inside a iframe
    */
-  iframe: React.PropTypes.bool,
+  iframe: PropTypes.bool,
   /**
    * Short urls generated
    */
-  links: React.PropTypes.object
+  links: PropTypes.object,
+  /**
+   * Define the category and action for the analytics
+   * event
+   */
+  analytics: PropTypes.shape({
+    category: PropTypes.string,
+    action: PropTypes.string
+  })
 };
 
 

app/scripts/components/SectionIntro/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ToolbarActions from '../ToolbarActions';
 
 function SectionIntro(props) {
@@ -32,6 +33,7 @@ function SectionIntro(props) {
               downloadUrl={props.downloadUrl}
               insightUrl={props.insightUrl}
               currentSection={props.currentSection}
+              analytics={props.analytics}
             />
             {props.children}
           </div>
@@ -50,23 +52,31 @@ SectionIntro.propTypes = {
    * - companyLogoUrl: string
    * Required
    */
-  data: React.PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
   /**
    * Define the insight url to the embed
    */
-  insightUrl: React.PropTypes.string,
+  insightUrl: PropTypes.string,
   /**
    * Define the download url to the dataset
    */
-  downloadUrl: React.PropTypes.string,
+  downloadUrl: PropTypes.string,
   /**
    * Define the current section
    */
-  currentSection: React.PropTypes.string,
+  currentSection: PropTypes.string,
   /**
    * Define the component childrens
    */
-  children: React.PropTypes.any
+  children: PropTypes.any,
+  /**
+   * Define the category and action for the analytics
+   * event of the share modal
+   */
+  analytics: PropTypes.shape({
+    category: PropTypes.string,
+    action: PropTypes.string
+  })
 };
 
 export default SectionIntro;

app/scripts/components/Thumbnails/Thumbnail.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { logEvent } from 'helpers/analytics';
 
 function Thumbnail(props) {
   const classes = ['c-thumbnail'];
@@ -8,7 +10,12 @@ function Thumbnail(props) {
   return (
     <div className={classes.join(' ')}>
       {props.url &&
-        <a href={props.url} target="_blank" rel="noopener noreferrer">
+        <a
+          href={props.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => props.analytics && logEvent(props.analytics.category, props.analytics.action, props.alt)}
+        >
           <img
             src={props.src}
             alt={props.alt}
@@ -33,19 +40,27 @@ Thumbnail.propTypes = {
    *  - "neutral": the color is grey
    * Default: no border (i.e. prop not defined)
    */
-  border: React.PropTypes.any,
+  border: PropTypes.any,
   /**
    * Define the link url
    */
-  url: React.PropTypes.string,
+  url: PropTypes.string,
   /**
    * Define the img src attribute
    */
-  src: React.PropTypes.string,
+  src: PropTypes.string,
   /**
    * Define the img alt attribute
    */
-  alt: React.PropTypes.string
+  alt: PropTypes.string,
+  /**
+   * Define the category and action for the analytics
+   * event
+   */
+  analytics: PropTypes.shape({
+    category: PropTypes.string,
+    action: PropTypes.string
+  })
 };
 
 export default Thumbnail;

app/scripts/components/ToolbarActions/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import { logEvent } from 'helpers/analytics';
 
 import ShareModal from '../Modal/ShareModal';
 import Button from '../Button/Button';
@@ -16,6 +18,10 @@ class ToolbarActions extends React.Component {
   }
 
   setShareModal(url, section) {
+    if (this.props.analytics) {
+      logEvent(this.props.analytics.category, this.props.analytics.action, 'Opens infowindow');
+    }
+
     this.setState({
       modalShare: true,
       shareUrl: url,
@@ -69,6 +75,7 @@ class ToolbarActions extends React.Component {
             url={this.state.shareUrl}
             opened={this.state.modalShare}
             close={() => this.setState({ modalShare: false })}
+            analytics={this.props.analytics}
           />
         </div>
       </div>
@@ -81,12 +88,20 @@ ToolbarActions.propTypes = {
    * Current section to use in the back button
    * Required
    */
-  currentSection: React.PropTypes.string.isRequired,
+  currentSection: PropTypes.string.isRequired,
   /**
    * Url to embed the insight
    */
   // insightUrl: React.PropTypes.string,
-  downloadUrl: React.PropTypes.string
+  downloadUrl: PropTypes.string,
+  /**
+   * Define the category and action for the analytics
+   * event of the share modal
+   */
+  analytics: PropTypes.shape({
+    category: PropTypes.string,
+    action: PropTypes.string
+  })
 };
 
 export default ToolbarActions;

commit 3b919dcb0186a15b8e8b19aaa9b5ff5e8b65ae66

Fix a regression from dad596a that prevented the user to share the maps

app/scripts/components/nexgddp-tool/tool-map/CompareMap.jsx
@@ -245,6 +245,7 @@ class CompareMap extends React.PureComponent {
                 }}
                 setOpen={this.props.setOpen}
                 setLinks={this.props.setLinks}
+                setAnalytics={shareModalActions.setAnalytics}
               />
             </Control>
           }

app/scripts/components/nexgddp-tool/tool-map/DifferenceMap.jsx
@@ -134,6 +134,7 @@ class DifferenceMap extends React.PureComponent {
                 }}
                 setOpen={this.props.setOpen}
                 setLinks={this.props.setLinks}
+                setAnalytics={shareModalActions.setAnalytics}
               />
             </Control>
           }

app/scripts/components/nexgddp-tool/tool-map/SimpleMap.jsx
@@ -138,6 +138,7 @@ class SimpleMap extends React.PureComponent {
                 }}
                 setOpen={this.props.setOpen}
                 setLinks={this.props.setLinks}
+                setAnalytics={shareModalActions.setAnalytics}
               />
             </Control>
           }

app/scripts/components/nexgddp-tool/tool-map/ToggleMap.jsx
@@ -145,6 +145,7 @@ class ToggleMap extends React.PureComponent {
               setBasemap={this.props.setBasemap}
               setLabels={this.props.setLabels}
               setBoundaries={this.props.setBoundaries}
+              setAnalytics={shareModalActions.setAnalytics}
             />
           </Control>
 

app/scripts/components/share-control/share-control-component.jsx
@@ -12,13 +12,17 @@ class ShareControl extends Component {
     const { open } = this.props;
     event.preventDefault();
     this.props.setOpen(!open);
-    this.props.setAnalytics({
-      category: 'Explore data',
-      action: 'Share a map'
-    });
     this.props.setLinks(this.props.links);
 
-    logEvent('Explore data', 'Share a map', 'Opens infowindow');
+    // Only for Explore
+    if (this.props.analytics) {
+      const { category, action } = this.props.analytics;
+      this.props.setAnalytics({
+        category,
+        action
+      });
+      logEvent(category, action, 'Opens infowindow');
+    }
   }
 
   render() {
@@ -46,7 +50,15 @@ ShareControl.propTypes = {
   links: PropTypes.object,
   setOpen: PropTypes.func,
   setLinks: PropTypes.func,
-  setAnalytics: PropTypes.func
+  setAnalytics: PropTypes.func.isRequired,
+  /**
+   * Define the category and action for the analytics
+   * event of the share modal
+   */
+  analytics: PropTypes.shape({
+    category: PropTypes.string,
+    action: PropTypes.string
+  })
 };
 
 export default ShareControl;

app/scripts/pages/explore/explore-map/explore-map-component.jsx
@@ -65,6 +65,10 @@ class ExploreMap extends PureComponent {
               }}
               setOpen={this.props.setOpen}
               setLinks={this.props.setLinks}
+              analytics={{
+                category: 'Explore data',
+                action: 'Share a map'
+              }}
             />
           }
         </Map>