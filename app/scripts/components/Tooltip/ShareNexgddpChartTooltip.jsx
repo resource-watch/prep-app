resource-watch/prep-app

commit 546e8ad846eb389b5dbe16aa5718189539859f8b

NexGDDP: embed chart; embed map

app/scripts/components/Tooltip/ShareNexgddpChartTooltip.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
 import { SaveWidgetModal, modalActions } from 'widget-editor';
+import * as shareModalActions from 'components/share-modal/share-modal-actions';
+
 
 // Redux
 import { toggleTooltip } from 'actions/tooltip';
@@ -11,6 +14,7 @@ class ShareNexgddpChartTooltip extends React.Component {
     super(props);
     this.onClickOutside = this.onClickOutside.bind(this);
     this.onClickSave = this.onClickSave.bind(this);
+    this.onClickShare = this.onClickShare.bind(this);
   }
 
   componentDidMount() {
@@ -41,12 +45,22 @@ class ShareNexgddpChartTooltip extends React.Component {
     });
   }
 
+  onClickShare() {
+    const { open, datasetSlug } = this.props;
+    const { origin, search } = window.location;
+    event.preventDefault();
+    this.props.setOpen(!open);
+    this.props.setLinks({
+      embed: `${origin}/embed/nexgddp/${datasetSlug}${search}&render=chart`
+    });
+  }
+
   render() {
     return (
       <div className="c-share-nexgddp-chart-tooltip">
         <ul>
           <li><button type="button" onClick={this.onClickSave}>Save widget</button></li>
-          {/* <li><button type="button">Share/Embed</button></li> */}
+          <li><button type="button" onClick={this.onClickShare}>Embed</button></li>
         </ul>
       </div>
     );
@@ -54,11 +68,15 @@ class ShareNexgddpChartTooltip extends React.Component {
 }
 
 ShareNexgddpChartTooltip.propTypes = {
+  open: PropTypes.bool,
   datasetId: PropTypes.string,
+  datasetSlug: PropTypes.string,
   toggleTooltip: PropTypes.func,
   getWidgetConfig: PropTypes.func,
   onClickCheckWidgets: PropTypes.func,
-  toggleEditorModal: PropTypes.func
+  toggleEditorModal: PropTypes.func,
+  setOpen: PropTypes.func,
+  setLinks: PropTypes.func
 };
 
 ShareNexgddpChartTooltip.defaultProps = {
@@ -68,12 +86,14 @@ ShareNexgddpChartTooltip.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-  datasetId: state.nexgddptool.dataset ? state.nexgddptool.dataset.id : null
+  datasetId: state.nexgddptool.dataset ? state.nexgddptool.dataset.id : null,
+  datasetSlug: state.nexgddptool.dataset ? state.nexgddptool.dataset.slug : null
 });
 
-const mapDispatchToProps = dispatch => ({
-  toggleTooltip: (...params) => dispatch(toggleTooltip(...params)),
-  toggleEditorModal: (...params) => dispatch(modalActions.toggleModal(...params))
-});
+const mapDispatchToProps = {
+  toggleTooltip,
+  ...modalActions,
+  ...shareModalActions
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShareNexgddpChartTooltip);

app/scripts/actions/nexgddptool.js
@@ -21,6 +21,7 @@ import {
   NEXGDDP_SET_BASEMAP,
   NEXGDDP_SET_LABELS,
   NEXGDDP_SET_BOUNDARIES,
+  NEXGDDP_SET_RENDER,
   NEXGDDP_SET_DATASET,
   NEXGDDP_SET_INDICATOR_DATASET,
   NEXGDDP_RESET_STATE
@@ -59,7 +60,8 @@ export function updateUrl() {
       labels: state.map.labels !== 'none'
         ? state.map.labels
         : undefined,
-      boundaries: !!state.map.boundaries || undefined
+      boundaries: !!state.map.boundaries || undefined,
+      render: state.render || undefined
     };
 
     const url = Object.keys(params)
@@ -393,6 +395,17 @@ export function setBoundaries(boundaries, changeUrl = true) {
   };
 }
 
+export function setRender(render, changeUrl = true) {
+  return (dispatch) => {
+    dispatch({
+      type: NEXGDDP_SET_RENDER,
+      payload: render
+    });
+
+    if (changeUrl) dispatch(updateUrl());
+  };
+}
+
 export function getUrlState() {
   return (dispatch, getState) => {
     const params = location.search
@@ -460,6 +473,9 @@ export function getUrlState() {
     }
     if (params.boundaries) {
       promises.push(dispatch(setBoundaries(params.boundaries === 'true')));
+    }
+    if (params.render) {
+      promises.push(dispatch(setRender(params.render)));
     }
 
     // Needed to chain the action

app/scripts/components/nexgddp-tool/NexGDDPTool.jsx
@@ -25,7 +25,7 @@ class NexGDDPTool extends React.PureComponent {
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.props.resetState()
       .then(() => this.props.setDataset(this.props.dataset))
       .then(() => this.props.getSelectorsInfo())
@@ -39,7 +39,15 @@ class NexGDDPTool extends React.PureComponent {
   }
 
   render() {
-    const { marker, isComparing, mapMode, indicatorDataset } = this.props;
+    const {
+      marker,
+      isComparing,
+      mapMode,
+      indicatorDataset,
+      render,
+      embed
+    } = this.props;
+
     const { loading } = this.state;
 
     return (
@@ -62,58 +70,77 @@ class NexGDDPTool extends React.PureComponent {
           </div>
         </div>
 
-        <div className="toolbar">
-          <div className="row">
-            <div className="columns small-12 medium-8">
-              { isComparing && (
-                <div>
-                  <button
-                    className={`c-button -inline ${mapMode === 'side-by-side' ? '-active' : ''}`}
-                    onClick={() => this.switchMapView('side-by-side')}
-                  >
-                    Side by side
-                  </button>
+        {(render === 'map' || !render) &&
+          <div className="toolbar">
+            <div className="row">
+              <div className="columns small-12 medium-8">
+                { isComparing && (
+                  <div>
+                    <button
+                      className={`c-button -inline ${mapMode === 'side-by-side' ? '-active' : ''}`}
+                      onClick={() => this.switchMapView('side-by-side')}
+                    >
+                      Side by side
+                    </button>
 
-                  <button
-                    className={`c-button -inline ${mapMode === 'toggle' ? '-active' : ''}`}
-                    onClick={() => this.switchMapView('toggle')}
-                  >
-                    Toggle
-                  </button>
+                    <button
+                      className={`c-button -inline ${mapMode === 'toggle' ? '-active' : ''}`}
+                      onClick={() => this.switchMapView('toggle')}
+                    >
+                      Toggle
+                    </button>
 
-                  <button
-                    className={`c-button -inline ${mapMode === 'difference' ? '-active' : ''}`}
-                    onClick={() => this.switchMapView('difference')}
-                  >
-                    Difference
-                  </button>
-                </div>
-              )}
-            </div>
-            <div className="columns small-12 medium-4">
-              <LocationSearch />
+                    <button
+                      className={`c-button -inline ${mapMode === 'difference' ? '-active' : ''}`}
+                      onClick={() => this.switchMapView('difference')}
+                    >
+                      Difference
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="columns small-12 medium-4">
+                <LocationSearch />
+              </div>
             </div>
           </div>
-        </div>
+        }
 
-        <div className="map">
+        {(render === 'map' || !render) &&
+          <div className="map">
+            <div className="row">
+              <div className="columns small-12">
+                {!isComparing &&
+                  <SimpleMap embed={embed} />
+                }
+
+                {(isComparing && mapMode === 'difference') &&
+                  <DifferenceMap embed={embed} />
+                }
+
+                {(isComparing && mapMode === 'side-by-side') &&
+                  <CompareMap embed={embed} />
+                }
+
+                {(isComparing && mapMode === 'toggle') &&
+                  <ToggleMap embed={embed} />
+                }
+              </div>
+            </div>
+          </div>
+        }
+
+        {!render && !marker && (
           <div className="row">
             <div className="columns small-12">
-              {!isComparing && <SimpleMap />}
-              {(isComparing && mapMode === 'difference') && <DifferenceMap />}
-              {(isComparing && mapMode === 'side-by-side') && <CompareMap />}
-              {(isComparing && mapMode === 'toggle') && <ToggleMap />}
+              <div className="help-text">
+                Click on the <span aria-label="Marker icon on the map"><Icon name="icon-marker" /></span> icon or search for a place to analyze in detail.
+              </div>
             </div>
-          </div>
-        </div>
-
-        {!marker && (
-          <div className="help-text">
-            Click on the <span aria-label="Marker icon on the map"><Icon name="icon-marker" /></span> icon or search for a place to analyze in detail.
           </div>
         )}
 
-        {marker && indicatorDataset && (
+        {(render === 'chart' || !render) && marker && indicatorDataset && (
           <div className="chart">
             <div className="row">
               <div className="columns small-12">
@@ -128,11 +155,13 @@ class NexGDDPTool extends React.PureComponent {
 }
 
 NexGDDPTool.propTypes = {
+  embed: PropTypes.bool,
   getSelectorsInfo: PropTypes.func,
   restoreState: PropTypes.func,
   setDefaultState: PropTypes.func,
   setMapMode: PropTypes.func,
   isComparing: PropTypes.bool,
+  render: PropTypes.oneOf(['map', 'chart', undefined]),
   marker: PropTypes.array,
   mapMode: PropTypes.oneOf(['difference', 'side-by-side', 'toggle']),
   indicatorDataset: PropTypes.object,
@@ -142,6 +171,8 @@ NexGDDPTool.propTypes = {
 };
 
 const mapStateToProps = state => ({
+  open: state.shareModal.open,
+  render: state.nexgddptool.render,
   marker: state.nexgddptool.marker,
   isComparing: !!state.nexgddptool.range2.selection,
   mapMode: state.nexgddptool.mapMode,

app/scripts/components/nexgddp-tool/tool-chart/TimeseriesChart.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import deepClone from 'lodash/cloneDeep';
 import { getConfig } from 'widget-editor';
-import './style.scss';
 
 // Redux
 import { setMarkerPosition } from 'actions/nexgddptool';
@@ -11,10 +10,12 @@ import { getIndicatorId, getIndicatorUnitSignal } from 'selectors/nexgddptool';
 import { toggleTooltip } from 'actions/tooltip';
 
 // Component
-import Vega from '../vega-chart/Vega';
 import Icon from 'components/ui/Icon';
 import Spinner from 'components/Loading/LoadingSpinner';
 import ShareNexgddpChartTooltip from 'components/Tooltip/ShareNexgddpChartTooltip';
+import Vega from '../vega-chart/Vega';
+
+import './style.scss';
 
 /* eslint-disable */
 const chartSpec = {
@@ -460,7 +461,7 @@ class TimeseriesChart extends React.PureComponent {
   }
 
   render() {
-    const { width, height, removeMarker, range1Selection, chartDataLoaded, chartDataError, datasetId } = this.props;
+    const { width, height, removeMarker, range1Selection, chartDataLoaded, chartDataError, datasetId, render } = this.props;
 
     // If for some reason, the range 1 is not selected or if the data
     // failed to load, we return
@@ -472,11 +473,22 @@ class TimeseriesChart extends React.PureComponent {
 
     return (
       <div className="c-tool-timeseries-chart">
-        { !chartDataLoaded && <Spinner inner transparent /> }
-        <button type="button" className="close-button" aria-label="Close chart" onClick={removeMarker}>
-          <Icon name="icon-cross" />
-        </button>
-        { chartDataLoaded &&
+        {!chartDataLoaded &&
+          <Spinner inner transparent />
+        }
+
+        {render !== 'chart' &&
+          <button
+            type="button"
+            className="close-button"
+            aria-label="Close chart"
+            onClick={removeMarker}
+          >
+            <Icon name="icon-cross" />
+          </button>
+        }
+
+        {chartDataLoaded &&
           <Vega
             width={width}
             height={height}
@@ -484,7 +496,8 @@ class TimeseriesChart extends React.PureComponent {
             spec={spec}
           />
         }
-        { chartDataLoaded && datasetId && canSave && (
+
+        {chartDataLoaded && datasetId && canSave && render !== 'chart' && (
           <div className="toolbar">
             <button type="button" className="share-button" onClick={e => this.onClickShare(e)} aria-label="Share/Save options">
               <Icon name="icon-share-dots" />
@@ -499,6 +512,7 @@ class TimeseriesChart extends React.PureComponent {
 TimeseriesChart.propTypes = {
   width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   height: PropTypes.number,
+  render: PropTypes.oneOf(['map', 'chart', undefined]),
   removeMarker: PropTypes.func,
   range1Selection: PropTypes.object,
   range2Selection: PropTypes.object,
@@ -517,6 +531,7 @@ TimeseriesChart.defaultProps = {
 };
 
 const mapStateToProps = state => ({
+  render: state.nexgddptool.render,
   range1Selection: state.nexgddptool.range1.selection,
   range2Selection: state.nexgddptool.range2.selection,
   chartDataLoaded: state.nexgddptool.chart.loaded,

app/scripts/components/nexgddp-tool/tool-map/CompareMap.jsx
@@ -12,10 +12,13 @@ import 'lib/leaflet-side-by-side/leaflet-side-by-side';
 // Redux
 import { getLayers, getRawLayers } from 'selectors/nexgddptool';
 import { setMarkerPosition, setMapZoom, setMapCenter, setBasemap, setBoundaries, setLabels, setMarkerMode } from 'actions/nexgddptool';
+import * as shareModalActions from 'components/share-modal/share-modal-actions';
 
-import BasemapControl from 'components/basemap-control';
-import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
+// Components
 import Legend from 'components/legend/index';
+import BasemapControl from 'components/basemap-control';
+import ShareControl from 'components/share-control/share-control-component';
+import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
 
 // Components
 import Icon from 'components/ui/Icon';
@@ -160,7 +163,7 @@ class CompareMap extends React.PureComponent {
   }
 
   render() {
-    const { map, marker, markerMode, range1Selection, range2Selection, rawLayers } = this.props;
+    const { dataset, embed, map, marker, markerMode, range1Selection, range2Selection, rawLayers } = this.props;
 
     // It will change center of map on marker location
     const mapOptions = Object.assign({}, mapDefaultOptions, {
@@ -175,6 +178,8 @@ class CompareMap extends React.PureComponent {
     const makerControlClassNames = classnames({
       '-active': markerMode
     });
+
+    const { origin, search } = window.location;
 
     return (
       <div className="c-tool-map">
@@ -230,6 +235,19 @@ class CompareMap extends React.PureComponent {
               setBoundaries={this.props.setBoundaries}
             />
           </Control>
+
+          {!embed &&
+            <Control position="bottomright">
+              <ShareControl
+                open={this.props.open}
+                links={{
+                  embed: `${origin}/embed/nexgddp/${(dataset || {}).slug}${search}&render=map`
+                }}
+                setOpen={this.props.setOpen}
+                setLinks={this.props.setLinks}
+              />
+            </Control>
+          }
         </Map>
 
         { !!rawLayers.length && (
@@ -245,6 +263,7 @@ class CompareMap extends React.PureComponent {
 }
 
 CompareMap.propTypes = {
+  dataset: PropTypes.object,
   map: PropTypes.shape({
     zoom: PropTypes.number,
     center: PropTypes.array,
@@ -255,6 +274,8 @@ CompareMap.propTypes = {
   rawLayers: PropTypes.array,
   marker: PropTypes.array,
   markerMode: PropTypes.bool,
+  open: PropTypes.bool,
+  embed: PropTypes.bool,
   range1Selection: PropTypes.object,
   range2Selection: PropTypes.object,
   setMarkerMode: PropTypes.func,
@@ -263,7 +284,9 @@ CompareMap.propTypes = {
   setMapCenter: PropTypes.func,
   setBasemap: PropTypes.func,
   setLabels: PropTypes.func,
-  setBoundaries: PropTypes.func
+  setBoundaries: PropTypes.func,
+  setOpen: PropTypes.func,
+  setLinks: PropTypes.func
 };
 
 const mapStateToProps = state => ({
@@ -273,7 +296,9 @@ const mapStateToProps = state => ({
   layers: getLayers(state),
   rawLayers: getRawLayers(state),
   range1Selection: state.nexgddptool.range1.selection,
-  range2Selection: state.nexgddptool.range2.selection
+  range2Selection: state.nexgddptool.range2.selection,
+  dataset: state.nexgddptool.dataset,
+  open: state.shareModal.open
 });
 
 const mapDispatchToProps = {
@@ -283,7 +308,8 @@ const mapDispatchToProps = {
   setMapCenter,
   setBasemap,
   setLabels,
-  setBoundaries
+  setBoundaries,
+  ...shareModalActions
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CompareMap);

app/scripts/components/nexgddp-tool/tool-map/DifferenceMap.jsx
@@ -11,11 +11,13 @@ import Control from 'react-leaflet-control';
 // Redux
 import { getLayers, getRawLayers } from 'selectors/nexgddptool';
 import { setMarkerPosition, setMapZoom, setMapCenter, setBasemap, setBoundaries, setLabels, setMarkerMode } from 'actions/nexgddptool';
+import * as shareModalActions from 'components/share-modal/share-modal-actions';
 
 // Components
-import BasemapControl from 'components/basemap-control';
-import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
 import Legend from 'components/legend/index';
+import BasemapControl from 'components/basemap-control';
+import ShareControl from 'components/share-control/share-control-component';
+import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
 
 import Icon from 'components/ui/Icon';
 
@@ -59,7 +61,7 @@ class DifferenceMap extends React.PureComponent {
   }
 
   render() {
-    const { map, marker, markerMode, layers, rawLayers } = this.props;
+    const { dataset, embed, map, marker, markerMode, layers, rawLayers } = this.props;
 
     // It will change center of map on marker location
     const mapOptions = Object.assign({}, mapDefaultOptions, {
@@ -76,6 +78,8 @@ class DifferenceMap extends React.PureComponent {
     const makerControlClassNames = classnames({
       '-active': markerMode
     });
+
+    const { origin, search } = window.location;
 
     return (
       <div className="c-tool-map">
@@ -120,6 +124,20 @@ class DifferenceMap extends React.PureComponent {
               setBoundaries={this.props.setBoundaries}
             />
           </Control>
+
+          {!embed &&
+            <Control position="bottomright">
+              <ShareControl
+                open={this.props.open}
+                links={{
+                  embed: `${origin}/embed/nexgddp/${(dataset || {}).slug}${search}&render=map`
+                }}
+                setOpen={this.props.setOpen}
+                setLinks={this.props.setLinks}
+              />
+            </Control>
+          }
+
         </Map>
 
         { !!rawLayers.length && (
@@ -135,6 +153,7 @@ class DifferenceMap extends React.PureComponent {
 }
 
 DifferenceMap.propTypes = {
+  dataset: PropTypes.object,
   map: PropTypes.shape({
     zoom: PropTypes.number,
     center: PropTypes.array
@@ -145,11 +164,15 @@ DifferenceMap.propTypes = {
   setMarkerPosition: PropTypes.func,
   setMapZoom: PropTypes.func,
   setMapCenter: PropTypes.func,
+  open: PropTypes.bool,
+  embed: PropTypes.bool,
   layers: PropTypes.array,
   rawLayers: PropTypes.array,
   setBasemap: PropTypes.func,
   setLabels: PropTypes.func,
-  setBoundaries: PropTypes.func
+  setBoundaries: PropTypes.func,
+  setOpen: PropTypes.func,
+  setLinks: PropTypes.func
 };
 
 const mapStateToProps = state => ({
@@ -157,7 +180,9 @@ const mapStateToProps = state => ({
   marker: state.nexgddptool.marker,
   markerMode: state.nexgddptool.markerMode,
   layers: getLayers(state),
-  rawLayers: getRawLayers(state)
+  rawLayers: getRawLayers(state),
+  dataset: state.nexgddptool.dataset,
+  open: state.shareModal.open
 });
 
 const mapDispatchToProps = {
@@ -167,7 +192,8 @@ const mapDispatchToProps = {
   setMapCenter,
   setBasemap,
   setLabels,
-  setBoundaries
+  setBoundaries,
+  ...shareModalActions
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DifferenceMap);

app/scripts/components/nexgddp-tool/tool-map/SimpleMap.jsx
@@ -10,12 +10,14 @@ import Control from 'react-leaflet-control';
 
 import { getLayers, getRawLayers } from 'selectors/nexgddptool';
 import { setMarkerPosition, setMapZoom, setMapCenter, setBasemap, setBoundaries, setLabels, setMarkerMode } from 'actions/nexgddptool';
+import * as shareModalActions from 'components/share-modal/share-modal-actions';
+
 import BasemapControl from 'components/basemap-control';
 import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
+import ShareControl from 'components/share-control/share-control-component';
 import Legend from 'components/legend/index';
 
 import Icon from 'components/ui/Icon';
-
 
 const mapDefaultOptions = {
   center: [20, -30],
@@ -57,7 +59,7 @@ class SimpleMap extends React.PureComponent {
   }
 
   render() {
-    const { map, marker, markerMode, layers, range1Selection, rawLayers } = this.props;
+    const { dataset, map, marker, markerMode, layers, range1Selection, rawLayers, embed } = this.props;
     const currentLayer = !!layers.length && layers[0];
 
     // It will change center of map on marker location
@@ -73,6 +75,8 @@ class SimpleMap extends React.PureComponent {
     const makerControlClassNames = classnames({
       '-active': markerMode
     });
+
+    const { origin, search } = window.location;
 
     return (
       <div className="c-tool-map">
@@ -125,6 +129,19 @@ class SimpleMap extends React.PureComponent {
             />
           </Control>
 
+          {!embed &&
+            <Control position="bottomright">
+              <ShareControl
+                open={this.props.open}
+                links={{
+                  embed: `${origin}/embed/nexgddp/${(dataset || {}).slug}${search}&render=map`
+                }}
+                setOpen={this.props.setOpen}
+                setLinks={this.props.setLinks}
+              />
+            </Control>
+          }
+
         </Map>
 
         { !!rawLayers.length && (
@@ -140,6 +157,7 @@ class SimpleMap extends React.PureComponent {
 }
 
 SimpleMap.propTypes = {
+  dataset: PropTypes.object,
   map: PropTypes.shape({
     zoom: PropTypes.number,
     center: PropTypes.array
@@ -149,22 +167,28 @@ SimpleMap.propTypes = {
   layers: PropTypes.array,
   rawLayers: PropTypes.array,
   range1Selection: PropTypes.object,
+  open: PropTypes.bool,
+  embed: PropTypes.bool,
   setMarkerPosition: PropTypes.func,
   setMarkerMode: PropTypes.func,
   setMapZoom: PropTypes.func,
   setMapCenter: PropTypes.func,
   setBasemap: PropTypes.func,
   setLabels: PropTypes.func,
-  setBoundaries: PropTypes.func
+  setBoundaries: PropTypes.func,
+  setOpen: PropTypes.func,
+  setLinks: PropTypes.func
 };
 
 const mapStateToProps = state => ({
+  dataset: state.nexgddptool.dataset,
   map: state.nexgddptool.map,
   marker: state.nexgddptool.marker,
   markerMode: state.nexgddptool.markerMode,
   layers: getLayers(state),
   rawLayers: getRawLayers(state),
-  range1Selection: state.nexgddptool.range1.selection
+  range1Selection: state.nexgddptool.range1.selection,
+  open: state.shareModal.open
 });
 
 const mapDispatchToProps = {
@@ -174,7 +198,8 @@ const mapDispatchToProps = {
   setMapCenter,
   setBasemap,
   setLabels,
-  setBoundaries
+  setBoundaries,
+  ...shareModalActions
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SimpleMap);

app/scripts/components/nexgddp-tool/tool-map/ToggleMap.jsx
@@ -11,11 +11,13 @@ import Control from 'react-leaflet-control';
 // Redux
 import { getLayers, getRawLayers } from 'selectors/nexgddptool';
 import { setMarkerPosition, setMapZoom, setMapCenter, setBasemap, setBoundaries, setLabels, setMarkerMode } from 'actions/nexgddptool';
+import * as shareModalActions from 'components/share-modal/share-modal-actions';
 
 // Components
-import BasemapControl from 'components/basemap-control';
-import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
 import Legend from 'components/legend/index';
+import BasemapControl from 'components/basemap-control';
+import ShareControl from 'components/share-control/share-control-component';
+import { basemapsSpec, labelsSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
 
 import Icon from 'components/ui/Icon';
 
@@ -68,7 +70,7 @@ class ToggleMap extends React.PureComponent {
   }
 
   render() {
-    const { map, marker, markerMode, layers, range1Selection, range2Selection, rawLayers } = this.props;
+    const { dataset, embed, map, marker, markerMode, layers, range1Selection, range2Selection, rawLayers } = this.props;
 
     // It will change center of map on marker location
     const mapOptions = Object.assign({}, mapDefaultOptions, {
@@ -83,6 +85,8 @@ class ToggleMap extends React.PureComponent {
     const makerControlClassNames = classnames({
       '-active': markerMode
     });
+
+    const { origin, search } = window.location;
 
     const currentLayer = !!layers.length && layers[this.state.index];
 
@@ -143,6 +147,20 @@ class ToggleMap extends React.PureComponent {
               setBoundaries={this.props.setBoundaries}
             />
           </Control>
+
+          {!embed &&
+            <Control position="bottomright">
+              <ShareControl
+                open={this.props.open}
+                links={{
+                  embed: `${origin}/embed/nexgddp/${(dataset || {}).slug}${search}&render=map`
+                }}
+                setOpen={this.props.setOpen}
+                setLinks={this.props.setLinks}
+              />
+            </Control>
+          }
+
         </Map>
 
         { !!rawLayers.length && (
@@ -169,13 +187,17 @@ ToggleMap.propTypes = {
   rawLayers: PropTypes.array,
   range1Selection: PropTypes.object,
   range2Selection: PropTypes.object,
+  open: PropTypes.bool,
+  embed: PropTypes.bool,
   setMarkerPosition: PropTypes.func,
   setMarkerMode: PropTypes.func,
   setMapZoom: PropTypes.func,
   setMapCenter: PropTypes.func,
   setBasemap: PropTypes.func,
   setLabels: PropTypes.func,
-  setBoundaries: PropTypes.func
+  setBoundaries: PropTypes.func,
+  setOpen: PropTypes.func,
+  setLinks: PropTypes.func
 };
 
 const mapStateToProps = state => ({
@@ -185,7 +207,9 @@ const mapStateToProps = state => ({
   layers: getLayers(state),
   rawLayers: getRawLayers(state),
   range1Selection: state.nexgddptool.range1.selection,
-  range2Selection: state.nexgddptool.range2.selection
+  range2Selection: state.nexgddptool.range2.selection,
+  dataset: state.nexgddptool.dataset,
+  open: state.shareModal.open
 });
 
 const mapDispatchToProps = {
@@ -195,7 +219,8 @@ const mapDispatchToProps = {
   setMapCenter,
   setBasemap,
   setLabels,
-  setBoundaries
+  setBoundaries,
+  ...shareModalActions
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToggleMap);

commit 483e17dc7744d8e89e53078254ceab52752118c5

Fix a regression from 546e8ad that prevented the user to share the graph

app/scripts/components/Tooltip/ShareNexgddpChartTooltip.jsx
@@ -35,7 +35,7 @@ class ShareNexgddpChartTooltip extends React.Component {
 
   onClickSave() {
     this.props.toggleTooltip(false);
-    this.props.toggleEditorModal(true, {
+    this.props.toggleModal(true, {
       children: SaveWidgetModal,
       childrenProps: {
         datasetId: this.props.datasetId,
@@ -74,7 +74,7 @@ ShareNexgddpChartTooltip.propTypes = {
   toggleTooltip: PropTypes.func,
   getWidgetConfig: PropTypes.func,
   onClickCheckWidgets: PropTypes.func,
-  toggleEditorModal: PropTypes.func,
+  toggleModal: PropTypes.func,
   setOpen: PropTypes.func,
   setLinks: PropTypes.func
 };