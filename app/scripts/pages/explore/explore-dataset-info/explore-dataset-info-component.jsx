94af5b66136de6474c23aea97db8898f51e325c4
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-02-01 15:53:04 +0100

    Explore: Remove areas from dataset info

--- app/scripts/pages/explore/explore-dataset-info/explore-dataset-info-component.jsx
+++ app/scripts/pages/explore/explore-dataset-info/explore-dataset-info-component.jsx
@@ -97,37 +97,6 @@ class DatasetInfo extends PureComponent {
         return false;
       });
 
-    datasetTags.filter(tag =>
-      GEOGRAPHIES.find(topic => topic.value === tag || (topic.children || []).find(child => child.value === tag)))
-      .map(tagValue =>
-        GEOGRAPHIES.forEach((_topic) => {
-          if (_topic.value === tagValue) {
-            areasList.push({ label: _topic.label, value: tagValue, key: 'geographies' });
-            return false;
-          }
-
-          if ((_topic.children || []).find(child => child.value === tagValue)) {
-            (_topic.children || []).forEach((child) => {
-              if (child.value === tagValue) {
-                areasList.push({ label: child.label, value: tagValue, key: 'geographies' });
-                return false;
-              } else if ((child.children || []).length) {
-                child.children.find((subchild) => {
-                  if (subchild.value === tagValue) {
-                    areasList.push({ label: subchild.label, value: tagValue, key: 'geographies' });
-                    return false;
-                  }
-                  return false;
-                });
-              }
-              return false;
-            });
-          }
-
-          return false;
-        })
-      );
-
     const LinkComponent = embed ? 'a' : Link;
 
     return (
