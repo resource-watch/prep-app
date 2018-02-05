be0707647ca9d3eb53d71f61717344a177e59f4f
Ian Denty <ian.denty@vizzuality.com>
2018-02-05 21:30:46 +0100

    Home:  Fix partner carousel button visibility on scroll

--- app/scripts/components/Button/arrow-button-component.jsx
+++ app/scripts/components/Button/arrow-button-component.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import Icon from 'components/ui/Icon';
+import classnames from 'classnames';
 
-const classes = 'c-button -filled -secondary-color -arrow -left';
 
-const ArrowButton = props => (
-  <button
-    className={`
-      ${classes} 
-      ${props.next ? '-next' : '-previous'}
-      ${props.currentSlide === 0 && !props.next ? '-hidden' : ''}
-      ${props.full ? '-full' : ''}
-      ${Math.ceil(props.slideCount / props.slidesToShow) === props.currentSlide + 1 && props.next ? '-hidden' : ''}
-    `}
-    onClick={props.onClick}
-  >
-    <Icon name="icon-arrow-down" />
-    <span className="arrow-label">{props.next ? 'next' : 'previous'}</span>
-  </button>
-);
+const ArrowButton = (props) => {
+  const classNames = classnames(
+    'c-button -filled -secondary-color -arrow -left',
+    {
+      '-next': !!props.next,
+      ' -previous': !props.next,
+      '-hidden': (!props.infinite && (props.next && Math.ceil(props.slideCount / props.slidesToShow)) === props.currentSlide + 1)
+        || (!props.infinite && (!props.next && props.currentSlide === 0)),
+      '-full': props.full
+    });
+
+  return (
+    <button
+      className={classNames}
+      onClick={props.onClick}
+    >
+      <Icon name="icon-arrow-down" />
+      <span className="arrow-label">{props.next ? 'next' : 'previous'}</span>
+    </button>
+  );
+};
 
 ArrowButton.propTypes = {
   next: React.PropTypes.bool,
   full: React.PropTypes.bool,
+  infinite: React.PropTypes.bool,
   onClick: React.PropTypes.func,
   currentSlide: React.PropTypes.number,
   slidesToShow: React.PropTypes.number,
