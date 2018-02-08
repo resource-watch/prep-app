39d7bbb3827bf3d527409bdc3abcf7312f1b2d3b
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-02-08 12:48:38 +0000

    Let the user create an embed widget with Explore's map

---
+++ app/scripts/containers/Modal/CreateEmbedWidget.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import CreateEmbedWidget from 'components/Modal/CreateEmbedWidget';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateEmbedWidget);
