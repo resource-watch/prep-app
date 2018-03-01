d279c7e8d0b523ef7c583998ec6d4d197123f000
Ian Denty <ian.denty@vizzuality.com>
2018-03-01 13:09:27 +0100

    Explore: Add tooltip to zoom to bounds legend item button

--- app/scripts/components/legend/legend-opacity-selector/legend-opacity-selector-component.jsx
+++ app/scripts/components/legend/legend-opacity-selector/legend-opacity-selector-component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -9,74 +9,62 @@ import Icon from 'components/ui/Icon';
 import 'rc-tooltip/assets/bootstrap_white.css';
 import './legend-opacity-selector-styles.scss';
 
-class LegendOpacitySelector extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      visibility: false
-    };
+const LegendOpacitySelector = (props) => {
+  const {
+    layerSpec,
+    onOpacityChange,
+    onVisibleChange,
+    visibility
+  } = props;
+  const { opacity, dataset } = layerSpec;
 
-    // BINDINGS //
-    this.onVisibleChange = this.onVisibleChange.bind(this);
-  }
+  const buttonClass = classnames({
+    '-active': visibility
+  });
 
-  onVisibleChange(visibility) {
-    this.setState({
-      visibility
-    });
-  }
+  const tooltipContent = (
+    <div className="tooltip-content">
+      <h5 className="title">Opacity</h5>
+      <SliderTooltip
+        options={{
+          defaultValue: opacity * 100,
+          min: 0,
+          max: 100,
+          step: 1
+        }}
+        onChange={value => onOpacityChange({ id: dataset, opacity: value / 100 })}
+        onClose={() => onVisibleChange(false)}
+      />
+    </div>
+  );
 
-  render() {
-    const { layerSpec, onOpacityChange } = this.props;
-    const { opacity, dataset } = layerSpec;
-    const { visibility } = this.state;
-
-    const buttonClass = classnames({
-      '-active': visibility
-    });
-
-    const tooltipContent = (
-      <div className="tooltip-content">
-        <h5 className="title">Opacity</h5>
-        <SliderTooltip
-          options={{
-            defaultValue: opacity * 100,
-            min: 0,
-            max: 100,
-            step: 1
-          }}
-          onChange={value => onOpacityChange({ id: dataset, opacity: value / 100 })}
-          onClose={() => this.setState({ visibility: false })}
-        />
-      </div>
-    );
-
-    return (
-      <div className="c-legend-opacity-selector">
-        <Tooltip
-          overlay={tooltipContent}
-          placement="bottom"
-          trigger="click"
-          overlayClassName="c-legend-opacity-selector"
-          onVisibleChange={this.onVisibleChange}
+  return (
+    <div className="c-legend-opacity-selector">
+      <Tooltip
+        overlay={tooltipContent}
+        placement="bottom"
+        trigger="click"
+        overlayClassName="c-legend-opacity-selector"
+        onVisibleChange={onVisibleChange}
+      >
+        <button
+          type="button"
+          onClick={() => !visibility && onVisibleChange(true)}
+          className={buttonClass}
         >
-          <button
-            type="button"
-            onClick={() => !visibility && this.setState({ visibility: true })}
-            className={buttonClass}
-          >
-            <Icon name="icon-opacity" className="-normal" />
-          </button>
-        </Tooltip>
-      </div>
-    );
-  }
-}
+          <Icon name="icon-opacity" className="-normal" />
+        </button>
+      </Tooltip>
+    </div>
+  );
+};
 
 LegendOpacitySelector.propTypes = {
+  visibility: PropTypes.bool,
   layerSpec: PropTypes.object,
-  onOpacityChange: PropTypes.func
+  onOpacityChange: PropTypes.func,
+  onVisibleChange: PropTypes.func
 };
 
 export default LegendOpacitySelector;
