9dd4d5be08f1145ebb51a708c67a613aabe60751
David Inga <david.inga@vizzuality.com>
2018-03-08 12:48:47 +0100

    explore loca toolbar

--- app/scripts/pages/explore/explore-datasets-list/explore-datasets-list-reducers.js
+++ app/scripts/pages/explore/explore-datasets-list/explore-datasets-list-reducers.js
@@ -155,9 +155,8 @@ export const setMultiActiveLayer = (state, { payload }) => {
   const { temporalResolution, period, scenario, id, layerId } = payload;
 
   const items = state.datasets.items.map((d) => {
-
     if (d.id === id && (d.provider === 'nexgddp' || d.provider === 'loca')) {
-      const currentLayer = d.metadata[0].info.nexgddp.layers.find(l =>
+      const currentLayer = d.metadata[0].info[d.provider].layers.find(l =>
         l.temp_resolution === temporalResolution.value &&
         l.scenario === scenario.value
       );
@@ -180,7 +179,7 @@ export const setMultiActiveLayer = (state, { payload }) => {
       }];
     }
 
-    if (d.id === id && (d.provider !== 'nexgddp' || d.provider !== 'loca')) {
+    if (d.id === id && (d.provider !== 'nexgddp' && d.provider !== 'loca')) {
       d.layer = d.layer.map(l => ({
         ...l,
         isActive: l.id === layerId
