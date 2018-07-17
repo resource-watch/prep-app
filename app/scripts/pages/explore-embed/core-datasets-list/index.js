5f5dc2616bf6ba4ea19dada4422a35f3a1fed40f
David Inga <david.inga@vizzuality.com>
2018-07-17 13:23:35 +0200

    fixing core datasets filter

--- app/scripts/pages/explore-embed/core-datasets-list/index.js
+++ app/scripts/pages/explore-embed/core-datasets-list/index.js
@@ -1,15 +1,16 @@
 import { connect } from 'react-redux';
 import Component from 'pages/explore/core-datasets-list/core-datasets-list-component';
 import initialState from 'pages/explore/core-datasets-list/core-datasets-list-initial-state';
+import * as reducers from 'pages/explore/core-datasets-list/core-datasets-list-reducers';
 import * as exploreActions from '../explore-embed-actions';
-import { getCoreDatasets } from './core-datasets-list-selector';
+import { getCoreDatasets, getParsedCoreDatasets } from './core-datasets-list-selector';
 
 const mapStateToProps = state => ({
   datasets: getCoreDatasets(state),
-  status: state.exploreEmbedPage.datasets.status,
+  coreDatasets: getParsedCoreDatasets(state),
   error: state.exploreEmbedPage.datasets.error,
   isFetching: state.exploreEmbedPage.datasets.isFetching
 });
 
-export { initialState, Component };
+export { initialState, reducers, Component };
 export default connect(mapStateToProps, exploreActions)(Component);
