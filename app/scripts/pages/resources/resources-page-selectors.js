5143364b91fd06c9cbe2da0c696e5127b7eda8e7
David Inga <david.inga@vizzuality.com>
2018-07-17 12:21:08 +0200

    resources in alphabetical order

--- app/scripts/pages/resources/resources-page-selectors.js
+++ app/scripts/pages/resources/resources-page-selectors.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 import groupBy from 'lodash/groupBy';
+import sortBy from 'lodash/sortBy';
 import { RESOURCES_CATEGORIES } from './resources-page-constants';
 
 const getResources = state => state.resourcePage.resources;
@@ -9,7 +10,7 @@ const allowedTypes = RESOURCES_CATEGORIES.map(category => category.value);
 export const getFilteredResources = createSelector(
   [getResources],
   (resources) => {
-    const filteredResources = resources.filter(resource => allowedTypes.includes(resource.resource_type));
+    const filteredResources = sortBy(resources.filter(resource => allowedTypes.includes(resource.resource_type)), (r) => r.title.trim());
     return groupBy(filteredResources, 'resource_type');
   }
 );
