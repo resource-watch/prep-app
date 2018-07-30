1ef3286494bdc79ac2ca331c9e862257c0b551c8
David Inga <david.inga@vizzuality.com>
2018-07-30 16:00:53 +0200

    added support for nexgddp and loca layers

--- app/scripts/pages/explore/explore-map/explore-map-component.jsx
+++ app/scripts/pages/explore/explore-map/explore-map-component.jsx
@@ -1,10 +1,10 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
-// import Map from 'components/map-vis';
 import classnames from 'classnames';
 import { getConfig } from 'widget-editor';
 import BasemapControl from 'components/basemap-control';
-import LegendControl from 'components/legend/legend-control';
+import LegendNexGDDPToolbar from 'components/legend/legend-nexgddp-toolbar';
+import LegendLOCAToolbar from 'components/legend/legend-loca-toolbar';
 import ShareControl from 'components/share-control';
 import SearchControl from 'components/search-control';
 import { basemapsSpec, labelsSpec, waterSpec, boundariesSpec } from 'components/basemap-control/basemap-control-constants';
@@ -14,23 +14,95 @@ import { PluginLeaflet } from 'layer-manager';
 
 class ExploreMap extends PureComponent {
 
+  static propTypes = {
+    basemap: PropTypes.oneOf(['default', 'dark', 'light', 'satellite', 'terrain']),
+    labels: PropTypes.oneOf(['none', 'dark', 'light']),
+    water: PropTypes.oneOf(['none', 'dark', 'light']),
+    activeLayers: PropTypes.arr,
+    activeLayersForMap: PropTypes.arr,
+    layersGroups: PropTypes.arr,
+    boundaries: PropTypes.bool,
+    zoom: PropTypes.number,
+    minZoom: PropTypes.number,
+    lat: PropTypes.number,
+    lng: PropTypes.number,
+    embed: PropTypes.bool,
+    embedExport: PropTypes.bool,
+    open: PropTypes.bool,
+    bbox: PropTypes.any,
+    sidebar: PropTypes.object,
+    setBasemap: PropTypes.func,
+    setLabels: PropTypes.func,
+    setBoundaries: PropTypes.func,
+    setWater: PropTypes.func,
+    setMapParams: PropTypes.func,
+    updateZIndex: PropTypes.func,
+    toggleInfo: PropTypes.func,
+    toggleDataset: PropTypes.func,
+    toggleVisibility: PropTypes.func,
+    updateOpacity: PropTypes.func,
+    setMultiActiveLayer: PropTypes.func,
+    setBBox: PropTypes.func,
+    setOpen: PropTypes.func,
+    setLinks: PropTypes.func,
+    setTab: PropTypes.func
+  }
+
+  static defaultProps = { activeLayers: [] }
+
+  constructor(props) {
+    super(props);
+    this.onSortChange = this.onSortChange.bind(this);
+  }
+
+  onSortChange(layers) {
+    console.log(layers);
+    const { updateZIndex } = this.props;
+     updateZIndex(layers);
+  }
+
+  setBoundaries() {
+    if (this.boundaries) this.map.removeLayer(this.boundaries);
+    const { boundaries } = this.props;
+    if (boundaries && Object.keys(boundaries).length) {
+      this.boundaries = L.tileLayer(boundaries.value, { ...boundaries.options, zIndex: 10001 });
+      this.map.addLayer(this.boundaries);
+    }
+  }
+
+  setWater() {
+    if (this.water) this.map.removeLayer(this.water);
+    const { water } = this.props;
+    if (water) {
+      this.water = L.tileLayer(water.value, { ...water.options, zIndex: 10002 });
+      this.map.addLayer(this.water);
+    }
+  }
+
+  getMapParams() {
+    const center = this.map.getCenter();
+    return { zoom: this.map.getZoom(), lat: center.lat, lng: center.lng };
+  }
+
   render() {
     const {
       setMapParams, setBBox, basemap, labels, water, boundaries, bbox, sidebar, zoom, lat, lng, minZoom, setBasemap,
-      setLabels, setBoundaries, setWater, activeLayers, activeLayersForMap, embed, embedExport, layersGroups
+      setLabels, setBoundaries, setWater, embed, embedExport, layersGroups, activeLayersForMap, activeLayers,
+      toggleVisibility, toggleInfo, updateOpacity, toggleDataset, setOpen, setLinks, setTab, setMultiActiveLayer
     } = this.props;
     const classNames = classnames({ '-embed': embed, '-embed-export': embedExport });
-    const left = (sidebar.open) ? 430 : 0;
+    const { open } = sidebar;
+    const left = open ? 430 : 0;
 
     // Map configuration
     const mapConfig = {
       basemap: {
         url: basemapsSpec[basemap].value,
-        options: basemapsSpec[basemap].options || {}
+        options: basemapsSpec[basemap].options || {}
       },
       label: {
         url: labelsSpec[labels].value,
-        options: labelsSpec[labels].options || {}
+        options: labelsSpec[labels].options || {}
       },
       mapOptions: Object.assign({}, {
         zoom,
@@ -38,6 +110,10 @@ class ExploreMap extends PureComponent {
         center: { lat, lng },
         zoomControl: false
       }),
+      events: {
+        zoomend: () => setMapParams(this.getMapParams()),
+        moveend: () => setMapParams(this.getMapParams())
+      },
       ...bbox && {
         bounds: {
           bbox,
@@ -52,55 +128,62 @@ class ExploreMap extends PureComponent {
     // Search control
     const { origin, search } = window.location;
 
+    const Toolbar = () => (
+      <LegendItemToolbar
+        onChangeVisibility={l => toggleVisibility({ id: l.dataset })}
+        onChangeInfo={l => toggleInfo({ id: l.dataset })}
+        onChangeOpacity={(l, opacity) => updateOpacity({ id: l.dataset, opacity })}
+        onRemoveLayer={l => toggleDataset({ id: l.dataset })}
+        onChangeBBox={(l) => {
+          setBBox(l.layerConfig.bbox);
+          // Reset the bounds inmediatly to have the possibility to click on it again
+          requestAnimationFrame(() => {
+            setBBox(null);
+          });
+        }}
+      />
+    );
+
     return (
       <div className={`c-explore-map ${classNames}`}>
         <Map {...mapConfig}>
-          {(map) => (
-            <Fragment>
-              <LayerManager map={map} plugin={PluginLeaflet}>
-                {activeLayers && activeLayers.map(l =>
-                  <Layer key={l.id} {...l} />
-                )}
-              </LayerManager>
-              <MapControls customClass="map-controls">
-                <ZoomControl map={map} customClass="zoom-control" />
-              </MapControls>
-            </Fragment>
-          )}
+          {(map) => {
+            this.map = map;
+            return (
+              <Fragment>
+                <LayerManager map={map} plugin={PluginLeaflet}>
+                  {activeLayersForMap && activeLayersForMap.map(l => <Layer key={l.id} {...l} />)}
+                </LayerManager>
+                <MapControls customClass="map-controls">
+                  <ZoomControl map={map} customClass="zoom-control" />
+                </MapControls>
+              </Fragment>
+            );
+          }}
         </Map>
 
         <Legend
           maxWidth={500}
           maxHeight={300}
-          layerGroups={layersGroups}
+          onChangeOrder={layers => this.onSortChange(layers)}
         >
           {layersGroups.map((lg, i) => (
             <LegendListItem
               index={i}
               key={lg.dataset}
               layerGroup={lg}
-              toolbar={
-                <LegendItemToolbar
-                  onChangeVisibility={l => this.props.toggleVisibility({ id: l.dataset })}
-                  onChangeInfo={l => this.props.toggleInfo({ id: l.dataset })}
-                  onChangeOpacity={(l, opacity) => this.props.updateOpacity({ id: l.dataset, opacity })}
-                  onRemoveLayer={l => this.props.toggleDataset({ id: l.dataset })}
-                  onChangeBBox={(l) => {
-                    this.props.setBBox(l.layerConfig.bbox);
-                    // Reset the bounds inmediatly to have the possibility to click on it again
-                    requestAnimationFrame(() => {
-                      this.props.setBBox(null);
-                    });
-                  }}
-                />
-              }
+              toolbar={<Toolbar />}
             >
+              {lg.layers[0].provider === 'nexgddp' &&
+                <LegendNexGDDPToolbar layerSpec={lg.layers[0]} onMultiLayer={l => setMultiActiveLayer(l)} />}
+              {lg.layers[0].provider === 'loca' &&
+                <LegendLOCAToolbar layerSpec={lg.layers[0]} onMultiLayer={l => setMultiActiveLayer(l)} />}
               <LegendItemTypes />
             </LegendListItem>
           ))}
         </Legend>
 
-        { !embedExport &&
+        { !embedExport && (
           <BasemapControl
             className="-absolute" // pfff....
             basemap={basemap}
@@ -112,12 +195,12 @@ class ExploreMap extends PureComponent {
             setWater={setWater}
             setBoundaries={setBoundaries}
           />
-        }
+        )}
 
-        {!(embed || embedExport) &&
+        {!(embed || embedExport) && (
           <ShareControl
             className="-absolute" // pfff....
-            open={this.props.open}
+            open={open}
             links={Object.assign(
               {
                 link: window.location.href,
@@ -137,198 +220,32 @@ class ExploreMap extends PureComponent {
                 }
                 : {}
             )}
-            setOpen={this.props.setOpen}
-            setLinks={this.props.setLinks}
-            setTab={this.props.setTab}
+            setOpen={setOpen}
+            setLinks={setLinks}
+            setTab={setTab}
             analytics={{
               category: 'Explore data',
               action: 'Share a map'
             }}
           />
-        }
+        )}
 
-        {!(embed || embedExport) &&
+        {!(embed || embedExport) && (
           <SearchControl
             className="-absolute -explore"
-            onChange={(bbox) => {
-              this.props.setBBox(bbox);
-
+            onChange={(nextBbox) => {
+              setBBox(nextBbox);
               // Reset the bounds inmediatly to have the possibility to click on it again
               requestAnimationFrame(() => {
-                this.props.setBBox(null);
+                setBBox(null);
               });
             }}
             open
           />
-        }
+        )}
       </div>
     );
   }
-
-  // constructor(props) {
-  //   super(props);
-  //   this.onSortChange = this.onSortChange.bind(this);
-  // }
-
-  // onSortChange(layers) {
-  //   this.props.updateZIndex(layers);
-  // }
-
-  // render() {
-  //   const {
-  //     setMapParams, setBBox, basemap, labels, water, boundaries, bbox, sidebar, zoom, lat, lng, minZoom, setBasemap,
-  //     setLabels, setBoundaries, setWater, activeLayers, activeLayersForMap, embed, embedExport
-  //   } = this.props;
-
-  //   const currentBasemap = basemapsSpec[basemap];
-  //   const currentLabels = labelsSpec[labels];
-  //   const currentBoundaries = boundaries ? boundariesSpec.dark : {};
-  //   const currentWater = waterSpec[water];
-  //   const currentMapOptions = Object.assign({}, {
-  //     zoom,
-  //     minZoom,
-  //     center: { lat, lng }
-  //   });
-
-  //   const classNames = classnames({ '-embed': embed, '-embed-export': embedExport });
-
-  //   const { origin, search } = window.location;
-
-  //   return (
-  //     <div className={`c-explore-map ${classNames}`}>
-  //       <Map
-  //         mapOptions={currentMapOptions}
-  //         basemap={currentBasemap}
-  //         labels={currentLabels}
-  //         boundaries={currentBoundaries}
-  //         water={currentWater}
-  //         layers={activeLayersForMap}
-  //         bbox={bbox}
-  //         sidebar={sidebar}
-  //         onChange={setMapParams}
-  //       >
-  //         { !embedExport &&
-  //           <BasemapControl
-  //             className="-absolute" // pfff....
-  //             basemap={basemap}
-  //             water={water}
-  //             labels={labels}
-  //             boundaries={boundaries}
-  //             setBasemap={setBasemap}
-  //             setLabels={setLabels}
-  //             setWater={setWater}
-  //             setBoundaries={setBoundaries}
-  //           />
-  //         }
-
-  //         {!(embed || embedExport) &&
-  //           <ShareControl
-  //             className="-absolute" // pfff....
-  //             open={this.props.open}
-  //             links={Object.assign(
-  //               {
-  //                 link: window.location.href,
-  //                 embed: `${origin}/embed/explore/${search}`
-  //               },
-  //               activeLayers.length && getConfig().userToken
-  //                 ? {
-  //                   widget: {
-  //                     url: `${origin}/embed/explore/${search}`,
-  //                     dataset: activeLayers[0].dataset,
-  //                     widgetLinks: activeLayers.slice(1)
-  //                       .map(d => ({
-  //                         name: d.name,
-  //                         link: `${origin}/dataset/${d.dataset}`
-  //                       }))
-  //                   }
-  //                 }
-  //                 : {}
-  //             )}
-  //             setOpen={this.props.setOpen}
-  //             setLinks={this.props.setLinks}
-  //             setTab={this.props.setTab}
-  //             analytics={{
-  //               category: 'Explore data',
-  //               action: 'Share a map'
-  //             }}
-  //           />
-  //         }
-
-  //         {!(embed || embedExport) &&
-  //           <SearchControl
-  //             className="-absolute -explore"
-  //             onChange={(bbox) => {
-  //               this.props.setBBox(bbox);
-
-  //               // Reset the bounds inmediatly to have the possibility to click on it again
-  //               requestAnimationFrame(() => {
-  //                 this.props.setBBox(null);
-  //               });
-  //             }}
-  //             open
-  //           />
-  //         }
-  //       </Map>
-
-  //       {!!activeLayers.length &&
-  //         <LegendControl
-  //           collapsed={embed}
-  //           embedExport={embedExport}
-  //           layersSpec={activeLayers}
-  //           position="topright"
-  //           onSortChange={this.onSortChange}
-  //           sortable
-  //           onVisibility={l => this.props.toggleVisibility({ id: l.dataset })}
-  //           onInfo={l => this.props.toggleInfo({ id: l.dataset })}
-  //           onOpacity={this.props.updateOpacity}
-  //           onClose={l => this.props.toggleDataset({ id: l.dataset })}
-  //           onMultiLayer={l => this.props.setMultiActiveLayer(l)}
-  //           onFitBounds={(l) => {
-  //             this.props.setBBox(l.layerConfig.bbox);
-
-  //             // Reset the bounds inmediatly to have the possibility to click on it again
-  //             requestAnimationFrame(() => {
-  //               this.props.setBBox(null);
-  //             });
-  //           }}
-  //         />}
-  //     </div>
-  //   );
-  // }
 }
-
-ExploreMap.defaultProps = { activeLayers: [] };
-
-ExploreMap.propTypes = {
-  basemap: PropTypes.oneOf(['default', 'dark', 'light', 'satellite', 'terrain']),
-  labels: PropTypes.oneOf(['none', 'dark', 'light']),
-  water: PropTypes.oneOf(['none', 'dark', 'light']),
-  activeLayers: PropTypes.array,
-  boundaries: PropTypes.bool,
-  zoom: PropTypes.number,
-  minZoom: PropTypes.number,
-  lat: PropTypes.number,
-  lng: PropTypes.number,
-  embed: PropTypes.bool,
-  embedExport: PropTypes.bool,
-  open: PropTypes.bool,
-  bbox: PropTypes.any,
-  sidebar: PropTypes.object,
-  setBasemap: PropTypes.func,
-  setLabels: PropTypes.func,
-  setBoundaries: PropTypes.func,
-  setWater: PropTypes.func,
-  setMapParams: PropTypes.func,
-  updateZIndex: PropTypes.func,
-  toggleInfo: PropTypes.func,
-  toggleDataset: PropTypes.func,
-  toggleVisibility: PropTypes.func,
-  updateOpacity: PropTypes.func,
-  setMultiActiveLayer: PropTypes.func,
-  setBBox: PropTypes.func,
-  setOpen: PropTypes.func,
-  setLinks: PropTypes.func,
-  setTab: PropTypes.func
-};
 
 export default ExploreMap;
