219596e624127854d0a93c26fb2151cecfa49fd4
David Inga <david.inga@vizzuality.com>
2018-09-27 12:16:06 +0200

    added ie11 support, some tests

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -65,21 +65,21 @@ module.exports = merge(sharedConfig, {
         return module.context && module.context.indexOf('node_modules') >= 0;
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true
-      },
-      output: { comments: false }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //     screw_ie8: true,
+    //     conditionals: true,
+    //     unused: true,
+    //     comparisons: true,
+    //     sequences: true,
+    //     dead_code: true,
+    //     evaluate: true,
+    //     if_return: true,
+    //     join_vars: true
+    //   },
+    //   output: { comments: false }
+    // }),
     new webpack.HashedModuleIdsPlugin(),
     new ExtractTextPlugin({
       filename: '[name].[contenthash].css',
