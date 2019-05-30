750a58d65a8ae0a3794b67d249cd6b4e94932e50
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2019-05-30 17:27:13 +0200

    Change temperature variables

--- public/embeds/timeline-exceedance/constants.js
+++ public/embeds/timeline-exceedance/constants.js
@@ -1,6 +1,6 @@
 const VARIABLES = [
   { id: 'precipitation', name: 'Precipitation'},
-  { id: 'tmax', name: 'Maximum Temperature'},
-  { id: 'tmin', name: 'Minimum Temperature'},
-  { id: 'tavg', name: 'Average Temperature'},
+  { id: 'tavg', name: '70° F for Average Temperature'},
+  { id: 'tmax', name: '95° F for Maximum Temperature'},
+  { id: 'tmin', name: '32° F for Minimum Temperature'},
 ];
