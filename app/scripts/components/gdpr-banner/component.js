c889b93cb430448d735ec4941f3543ca18c01e0f
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2019-08-29 12:11:59 +0200

    GDPR: Style fix

--- app/scripts/components/gdpr-banner/component.js
+++ app/scripts/components/gdpr-banner/component.js
@@ -7,29 +7,27 @@ import { Link } from 'react-router';
 
 const GDPRBanner = ({ handleGDPR }) => (
   <div className="c-gdpr-banner">
-    <div className="l-container">
-      <div className="row">
-        <div className="column small-9 medium-10">
-          This website uses cookies to provide you with an improved user experience. By
-          continuing to browse this site, you consent to the use of cookies and similar
-          technologies. Please visit our
-          {' '}
-          <Link to="/privacy-policy">
-            privacy policy
-          </Link>
-          {' '}
-          for further details.
-        </div>
-        <div className="column small-3 medium-2">
-          <div className="c-button-container -j-end -a-center -full-height">
-            <button
-              type="button"
-              className="c-new-button -primary -alt -compressed -fs-medium"
-              onClick={handleGDPR}
-            >
-              I agree
-            </button>
-          </div>
+    <div className="row">
+      <div className="column small-9 medium-10">
+        This website uses cookies to provide you with an improved user experience. By
+        continuing to browse this site, you consent to the use of cookies and similar
+        technologies. Please visit our
+        {' '}
+        <Link to="/privacy-policy">
+          privacy policy
+        </Link>
+        {' '}
+        for further details.
+      </div>
+      <div className="column small-3 medium-2">
+        <div className="c-button-container -j-end -a-center -full-height">
+          <button
+            type="button"
+            className="c-new-button -primary -alt -compressed -fs-medium"
+            onClick={handleGDPR}
+          >
+            I agree
+          </button>
         </div>
       </div>
     </div>
