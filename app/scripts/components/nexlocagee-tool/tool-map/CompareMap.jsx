e54e967eea57e31428f391c355d966cd0f0baad3
David Inga <david.inga@vizzuality.com>
2021-04-06 12:30:08 +0200

    fixed current layer for map

--- app/scripts/components/nexlocagee-tool/tool-map/CompareMap.jsx
+++ app/scripts/components/nexlocagee-tool/tool-map/CompareMap.jsx
@@ -6,7 +6,7 @@ import { Map, TileLayer, ZoomControl, Marker } from 'react-leaflet';
 import Control from 'react-leaflet-control';
 
 // Redux
-import { getLayers, getRawLayers } from 'selectors/nexlocageetool';
+import { getActiveLayers, getActiveRawLayers } from 'selectors/nexlocageetool';
 import { setMarkerPosition, setMapZoom, setMapCenter, setBasemap, setBoundaries, setLabels, setWater, setMarkerMode } from 'actions/nexlocageetool';
 import * as shareModalActions from 'components/share-modal/share-modal-actions';
 import { toggleTooltip } from 'actions/tooltip';
@@ -353,8 +353,8 @@ const mapStateToProps = state => ({
   map: state.nexlocageetool.map,
   marker: state.nexlocageetool.marker,
   markerMode: state.nexlocageetool.markerMode,
-  layers: getLayers(state),
-  rawLayers: getRawLayers(state),
+  layers: getActiveLayers(state),
+  rawLayers: getActiveRawLayers(state),
   range1Selection: state.nexlocageetool.range1.selection,
   range2Selection: state.nexlocageetool.range2.selection,
   dataset: state.nexlocageetool.dataset,
--- app/scripts/selectors/nexlocageetool.js
+++ app/scripts/selectors/nexlocageetool.js
@@ -1,20 +1,6 @@
 import 'whatwg-fetch';
 import { createSelector } from 'reselect';
 
-// Temporal code
-const mapIndicatorToUnitSignal = {
-  hdds: { from: 'Degrees', to: 'Degrees ºC', value: 1, type: 'factor' },
-  cdds: { from: 'Degrees', to: 'Degrees ºC', value: 1, type: 'factor' },
-  cum_pr: { from: 'Kg*m-2*s-1', to: 'mm', value: 86400, type: 'factor' },
-  dry: { from: 'Nº 5-day periods', to: 'Nº 5-day periods', value: 1, type: 'factor' },
-  tasmax: { from: 'Kelvin', to: 'ºC', value: 273.23, type: 'minus' },
-  tasavg: { from: 'Kelvin', to: 'ºC', value: 273.23, type: 'minus' },
-  xpr: { from: 'Days', to: 'Days', value: 1, type: 'factor' },
-  xs: { from: 'Days', to: 'Days', value: 1, type: 'factor' },
-  ffs: { from: 'Days', to: 'Days', value: 1, type: 'factor' },
-  tasmin: { from: 'Kelvin', to: 'ºC', value: 273.23, type: 'minus' }
-};
-
 const state = state => state; // eslint-disable-line no-shadow
 const mapMode = ({ nexlocageetool }) => nexlocageetool.mapMode;
 const scenario = ({ nexlocageetool }) => nexlocageetool.scenario;
@@ -22,60 +8,61 @@ const range1Selection = ({ nexlocageetool }) => nexlocageetool.range1.selection;
 const range2Selection = ({ nexlocageetool }) => nexlocageetool.range2.selection;
 const dataset = ({ nexlocageetool }) => nexlocageetool.dataset || null;
 const additionalData = ({ datasetPage }) => datasetPage.additionalData || null;
-const layers = ({ nexlocageetool }) => nexlocageetool.dataset ? nexlocageetool.dataset.layer : [];
 
-export const getLayers = createSelector(
+export const getActiveRawLayers = createSelector(
   dataset,
-  mapMode,
-  scenario,
   range1Selection,
   range2Selection,
+  mapMode,
+  scenario,
   additionalData,
-  (dataset, mapMode, scenario, range1Selection, range2Selection, additionalData) => { // eslint-disable-line no-shadow
+  (dataset, range1Selection, range2Selection, mapMode, scenario, additionalData) => { // eslint-disable-line no-shadow
     if ((!range1Selection && !range2Selection) || !dataset || !additionalData || !dataset.layer.length) return [];
     const { layer } = dataset;
     const activeLayers = [];
 
     if (mapMode === 'difference') {
-      const range1Date = `${range1Selection.value}`;
       const { layer: additionalLayers } = additionalData.change[scenario.selection.value];
       const currentLayer = additionalLayers.find(({ layerConfig }) => layerConfig.order === range1Selection.value);
 
-      activeLayers.push({
-        url: `${config.apiUrlRW}/layer/${currentLayer.id}/tile/gee/{z}/{x}/{y}`,
-        date: range1Date,
-      });
+      activeLayers.push(currentLayer);
 
       return activeLayers;
     }
 
     if (range1Selection) {
       const currentLayer1 = layer.find(({ layerConfig }) => layerConfig.order === range1Selection.value);
-      const range1Date = `${range1Selection.value}`;
-      activeLayers.push({
-        url: `${config.apiUrlRW}/layer/${currentLayer1.id}/tile/gee/{z}/{x}/{y}`,
-        date: range1Date
-      });
+      activeLayers.push(currentLayer1);
     }
 
     if (range2Selection) {
       const currentLayer2 = layer.find(({ layerConfig }) => layerConfig.order === range2Selection.value);
-      const range2Date = `${range2Selection.value}`;
-      activeLayers.push({
-        url: `${config.apiUrlRW}/layer/${currentLayer2.id}/tile/gee/{z}/{x}/{y}`,
-        date: range2Date
-      });
+      activeLayers.push(currentLayer2);
     }
 
     return activeLayers;
   }
 );
 
-export const getRawLayers = createSelector(
-  layers,
-  (layers) => { // eslint-disable-line no-shadow
-    if (!layers.length) return [];
-    return layers.slice(0, 1);
+export const getActiveLayers = createSelector(
+  getActiveRawLayers,
+  range1Selection,
+  range2Selection,
+  (activeRawLayers, range1Selection, range2Selection) => { // eslint-disable-line no-shadow
+    let activeLayers = activeRawLayers && activeRawLayers.length ? activeRawLayers.filter((layer) => !!layer) : [];
+
+    if ((!range1Selection && !range2Selection) || !activeLayers.length) return [];
+
+    const range1Date = range1Selection ? `${range1Selection.value}` : null;
+    const range2Date = range2Selection ? `${range2Selection.value}` : null;
+
+
+    activeLayers = activeRawLayers.map((layer) => ({
+      url: `${config.apiUrlRW}/layer/${layer.id}/tile/gee/{z}/{x}/{y}`,
+      date: range2Selection ? range2Date : range1Date,
+    }));
+
+    return activeLayers;
   }
 );
 
