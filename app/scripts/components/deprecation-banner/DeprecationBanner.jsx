c6fdb2e16c816c71836b77b42a727c792b546e54
Tim Anderegg <timothy.anderegg@gmail.com>
2026-09-08 12:33:37 -0400

    Typo in class name.

--- app/scripts/components/deprecation-banner/DeprecationBanner.jsx
+++ app/scripts/components/deprecation-banner/DeprecationBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DeprecationBanner = ({ children }) => (
-  <div role="alert" className=".banner">
+  <div role="alert" className="banner">
     {children}
   </div>
 );
